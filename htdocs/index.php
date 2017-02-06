1f3413a62cffc435955065d7f426879609ace5dd
Micz Flor <micz.flor@web.de>
2017-02-06 16:25:43 +0100

    changed typos

--- htdocs/index.php
+++ htdocs/index.php
@@ -114,29 +114,25 @@ html_bootstrap3_createHeader("en","RPi Jukebox",$conf['base_url']);
 
     <div class="row">
       <div class="col-lg-12">
-  <?php
-  $volume = system("/usr/bin/sudo amixer get Master | awk '$0~/%/{print $4}' | tr -d '[]'");
-  print $volume;
-  ?>
-  <div class="well">
-      <h4>Select Volume</h4>
-
-<form name='volume' method='post' action='<?php print $_SERVER['PHP_SELF']; ?>'>
-  <select name='volume'>
-     <option value='0'>Mute (0%)</option>
-     <option value='30'>30%</option>
-     <option value='50'>50%</option>
-     <option value='75'>75%</option>
-     <option value='80'>80%</option>
-     <option value='85'>85%</option>
-     <option value='90'>90%</option>
-     <option value='95'>95%</option>
-     <option value='100'>100%</option>
-  </select>
-<input type='submit' name='submit' value='Set volume'/>
-</form>
-
-  </div>
+          <div class="well">
+              <h4>Select Volume</h4>
+        
+                <form name='volume' method='post' action='<?php print $_SERVER['PHP_SELF']; ?>'>
+                  <select name='volume'>
+                     <option value='0'>Mute (0%)</option>
+                     <option value='30'>30%</option>
+                     <option value='50'>50%</option>
+                     <option value='75'>75%</option>
+                     <option value='80'>80%</option>
+                     <option value='85'>85%</option>
+                     <option value='90'>90%</option>
+                     <option value='95'>95%</option>
+                     <option value='100'>100%</option>
+                  </select>
+                <input type='submit' name='submit' value='Set volume'/>
+                </form>
+        
+          </div>
         
   <h2>Available audio</h2>
 <?php
