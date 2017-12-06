9067d15c07f59533a32dfd524843d79858020063
Micz Flor <micz.flor@web.de>
2017-12-06 13:20:07 +0100

    adding install and sample files for Access Point installer

--- scripts/installscripts/jessie-install-AccessPoint-01.sh
+++ scripts/installscripts/jessie-install-AccessPoint-01.sh
@@ -2,13 +2,19 @@
 #
 # see https://github.com/MiczFlor/RPi-Jukebox-RFID for details
 #
-# this script installs my personal settings on a fresh RPi install
+# this script installs default settings from the github repo to run
+# the RPi jukebox like an Access Point to connect over wifi without
+# a router.
 # before running the script, the RPi needs to be set up, see INSTALL.md on github
 # download the install script only to home dir and run
-# Don't run as sudo. Simply type ./jessie-install-default-01.sh
+# Don't run as sudo. Simply type ./jessie-install-AccessPoint-01.sh
 
-# If you want to make this work with your wifi network, change the following files before you run this script:
-# dhcpcd.conf.jessie-default.sample
+# Backup Access point files like this:
+#sudo cp /etc/network/interfaces /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/interfaces.jessie-WlanAP.sample
+#sudo cp /etc/dhcpcd.conf /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/dhcpcd.conf.jessie-WlanAP.sample
+#sudo cp /etc/dnsmasq.conf /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/dnsmasq.conf.jessie-WlanAP.sample
+#sudo cp /etc/hostapd/hostapd.conf /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/hostapd.conf.jessie-WlanAP.sample
+#sudo cp /etc/default/hostapd /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/hostapd.jessie-WlanAP.sample
 
 # Install packages
 sudo apt-get update
@@ -26,11 +32,35 @@ sudo sed -i 's/geteuid/getppid/' /usr/bin/vlc
 # COPY CONFIG PRESETS TO LIVE FOLDERS
 #####################################
 
+# DHCP network interfaces
+# -rw-r--r-- 1 root root 192 Nov 27 15:39 /etc/network/interfaces
+sudo cp /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/interfaces.jessie-WlanAP.sample /etc/network/interfaces
+sudo chown root:root /etc/network/interfaces
+sudo chmod 644 /etc/network/interfaces
+
 # DHCP configuration settings
 # -rw-rw-r-- 1 root netdev 1371 Nov 17 21:02 /etc/dhcpcd.conf
-sudo cp /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/dhcpcd.conf.jessie-default.sample /etc/dhcpcd.conf
+sudo cp /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/dhcpcd.conf.jessie-WlanAP.sample /etc/dhcpcd.conf
 sudo chown root:netdev /etc/dhcpcd.conf
 sudo chmod 664 /etc/dhcpcd.conf
+
+# dnsmasq configuration settings
+# -rw-r--r-- 1 root root 26040 Nov 26 23:37 /etc/dnsmasq.conf
+sudo cp /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/dnsmasq.conf.jessie-WlanAP.sample /etc/dnsmasq.conf
+sudo chown root:root /etc/dnsmasq.conf
+sudo chmod 644 /etc/dnsmasq.conf
+
+# hostapd configuration settings
+# -rw------- 1 root root 261 Nov 26 23:40 /etc/hostapd/hostapd.conf
+sudo cp /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/hostapd.conf.jessie-WlanAP.sample /etc/hostapd/hostapd.conf
+sudo chown root:root /etc/hostapd/hostapd.conf
+sudo chmod 600 /etc/hostapd/hostapd.conf
+
+# default hostapd settings
+# rw-r--r-- 1 root root 825 Nov 26 23:45 /etc/default/hostapd
+sudo cp /home/pi/RPi-Jukebox-RFID/misc/sampleconfigs/hostapd.conf.jessie-WlanAP.sample /etc/default/hostapd
+sudo chown root:root /etc/default/hostapd
+sudo chmod 644 /etc/default/hostapd
 
 # Samba configuration settings
 # -rw-r--r-- 1 root root 9416 Nov 17 21:04 /etc/samba/smb.conf
@@ -91,4 +121,6 @@ sudo systemctl enable dhcpcd
 # you must use password 'raspberry' because this is 
 # expected in the smb.conf file
 sudo smbpasswd -a pi
+
+
 
