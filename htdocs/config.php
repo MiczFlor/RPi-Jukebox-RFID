9c3bff3b9cb2cc079a9181bd299802ce14afce4b
Micz Flor <micz.flor@web.de>
2017-12-23 15:08:58 +0100

    conf file as sample

--- htdocs/config.php
+++ htdocs/config.php
@@ -4,9 +4,4 @@ $conf               = array();
 $conf['base_url']   = ""; // usually => "", generally: end with trailing slash
 $conf['base_path']  = "/home/pi/RPi-Jukebox-RFID"; // absolute path to folder
 
-/**/
-$conf['base_path']  = "/home/micz/Documents/github/RPi-Jukebox-RFID"; // absolute path to folder
-/**/
-
-
 ?>
