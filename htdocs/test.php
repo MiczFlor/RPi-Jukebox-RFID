1e8532ae27c9607f76da6ce73d8c140e2a2c0325
Micz Flor <micz.flor@web.de>
2018-01-08 11:54:42 +0100

    test.php for server conf

---
+++ htdocs/test.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>PHP Test</title>
+</head>
+<body>
+<pre>
+
+<?php
+
+if(file_exists("config.php")) {
+    include("config.php");
+    print "File 'config.php' found and included.\n";
+} else {
+    print "File 'config.php' not found.\n";
+}
+
+$conf['url_abs']    = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']; // URL to PHP_SELF
+
+print "\nVariable \$conf['url_abs'] = ".$conf['url_abs']."\n";
+
+if(file_exists("func.php")) {
+    include("func.php");
+    print "\nFile 'func.php' found and included.\n";
+} else {
+    print "\nFile 'func.php' not found.\n";
+}
+
+if (function_exists('html_bootstrap3_createHeader')) {
+    echo "\nFunction 'html_bootstrap3_createHeader' exists.\n";
+} else {
+    echo "\nFunction 'html_bootstrap3_createHeader' does not exist.\n";
+}
+
+print "\nCalling 'html_bootstrap3_createHeader(\"en\",\"RPi Jukebox\",".$conf['base_url'].");'\n";
+
+// read the shortcuts used
+$shortcutstemp = array_filter(glob($conf['base_path'].'/shared/shortcuts/*'), 'is_file');
+$shortcuts = array(); // the array with pairs of ID => foldername
+// read files' content into array
+foreach ($shortcutstemp as $shortcuttemp) {
+    $shortcuts[basename($shortcuttemp)] = trim(file_get_contents($shortcuttemp));
+}
+print "\nShortcuts temporary file:\n";
+print_r($shortcutstemp); print "\n";
+print "\nShortcuts resolved file:\n";
+print_r($shortcuts); print "\n";
+
+// read the subfolders of shared/audiofolders
+$audiofolders = array_filter(glob($conf['base_path'].'/shared/audiofolders/*'), 'is_dir');
+print "\nAudio folders:\n";
+print "<pre>"; print_r($audiofolders); print "\n";
+
+?>
+</pre>
+
+
+</body>
+</html>
