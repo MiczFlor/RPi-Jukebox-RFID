14f4ef2ed358aaf8710b2fdbdcba3b38aed7d161
Erich Ebenhoch <erich.ebenhoch@tttech.com>
2018-04-07 15:18:07 +0200

    reader script does not exit, when file not found check is done by daemon

--- scripts/Reader.py
+++ scripts/Reader.py
@@ -7,34 +7,42 @@ import sys
 
 from evdev import InputDevice, categorize, ecodes, list_devices
 from select import select
+
+
 class Reader:
+
+	self.deviceName = None
+	self.dev = None
+	
+	# Mapping table for Even.Code from reader
+	self.keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
+	
 	def __init__(self):
 		path = os.path.dirname(os.path.realpath(__file__))
-		self.keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
-		if not os.path.isfile(path + '/deviceName.txt'):
-			sys.exit('Please run config.py first')
-		else: 
+		
+		# check if device has been selected
+		# file is created by using script RegisterDevice.py
+		if os.path.isfile(os.path.join(path, '/deviceName.txt'):
+			
 			with open(path + '/deviceName.txt','r') as f:
-				deviceName = f.read()
+				self.deviceName = f.read()
+			
+			# select device by name
 			devices = [InputDevice(fn) for fn in list_devices()]
 			for device in devices:
-				if device.name == deviceName:
+				if device.name == self.deviceName:
 					self.dev = device
-					break 		
-			try:
-				self.dev
-			except:
-				sys.exit('Could not find the device %s\n. Make sure is connected' % deviceName)
+					break
+			#end for
 		
 	def readCard(self):
-		stri=''
-		key = ''
+		stri = ''
+		key  = ''
 		while key != 'KEY_ENTER':
 		   r,w,x = select([self.dev], [], [])
 		   for event in self.dev.read():
 			if event.type==1 and event.value==1:
 				stri+=self.keys[ event.code ]
-				#print( keys[ event.code ] )
 				key = ecodes.KEY[ event.code ]
 		return stri[:-1]
 
--- scripts/daemon_rfid_reader.py
+++ scripts/daemon_rfid_reader.py
@@ -16,11 +16,7 @@ console.setFormatter(console_format)
 # add the handler to the root logger
 logging.getLogger('').addHandler(console)
 
-# Setup reader object
-reader = Reader()
 
-# get absolute path of this script
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Handler if the process is killed (by OS during shutdown most probably)
 def sigterm_handler(signal, frame):
@@ -31,9 +27,7 @@ def sigterm_handler(signal, frame):
     sys.exit(0)
 # end def sigterm_handler
 
-# register SIGTERM and SIGINT handler
-signal.signal(signal.SIGTERM, sigterm_handler)
-signal.signal(signal.SIGINT, sigterm_handler)
+
 
 # function to kill a VLC processes
 def check_kill_process(pstring):
@@ -45,57 +39,77 @@ def check_kill_process(pstring):
 #end def check_kill_process
         
 
-# Lets start the daemon and wait for RFID
-while True:
-	# reading the card id - blocking call
-	cardid = reader.readCard()
-	# cardid = input("ID: ")
 
-	logging.info("Card ID %s was used.", cardid )
-	# TODO log to file -  > $PATHDATA/../shared/latestID.txt
+# register SIGTERM and SIGINT handler
+signal.signal(signal.SIGTERM, sigterm_handler)
+signal.signal(signal.SIGINT, sigterm_handler)
 
-	music_folder  = os.path.join(dir_path, '../shared/audiofolders', str(cardid) )
-	playlist_file = os.path.join(dir_path, '../playlists', str(cardid) + '.m3u' )
+# Setup reader object
+reader = Reader()
 
-	# Expected folder structure:
-	#
-	# $dir_path + /../shared/audiofolders/ + cardid
-	# $dir_path + /../shared/playlists/ + cardid + ".m3u"
+# Check if devie found
+if reader.dev != None:
 
-	# if a music_folder , change playlist- else just print warning
-	
-	if (os.path.exists( music_folder ) ):
-		# create playlist in the folder 
-		musicfiles = [f for f in os.listdir(music_folder) if os.path.isfile(os.path.join(music_folder, f))]
+	# get absolute path of this script
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+			
+	# Lets start the daemon and wait for RFID
+	while True:
+		# reading the card id - blocking call
+		cardid = reader.readCard()
+		# cardid = input("ID: ")
+
+		logging.info("Card ID %s was used.", cardid )
+		# TODO log to file -  > $PATHDATA/../shared/latestID.txt
+
+		music_folder  = os.path.join(dir_path, '../shared/audiofolders', str(cardid) )
+		playlist_file = os.path.join(dir_path, '../playlists', str(cardid) + '.m3u' )
+
+		# Expected folder structure:
+		#
+		# $dir_path + /../shared/audiofolders/ + cardid
+		# $dir_path + /../shared/playlists/ + cardid + ".m3u"
+
+		# if a music_folder , change playlist- else just print warning
 		
-		logging.info("Check %s for music.", music_folder )
-		# check if files are there
-		if (musicfiles != []):
-			# create new play list
-			playlist = open(playlist_file, 'w')
+		if (os.path.exists( music_folder ) ):
+			# create playlist in the folder 
+			musicfiles = [f for f in os.listdir(music_folder) if os.path.isfile(os.path.join(music_folder, f))]
 			
-			for file in musicfiles:
-				playlist.write(os.path.join('../shared/audiofolders', str(cardid), '%s\n' % (file)))
-			playlist.close()
+			logging.info("Check %s for music.", music_folder )
+			# check if files are there
+			if (musicfiles != []):
+				# create new play list
+				playlist = open(playlist_file, 'w')
+				
+				for file in musicfiles:
+					playlist.write(os.path.join('../shared/audiofolders', str(cardid), '%s\n' % (file)))
+				playlist.close()
+				
+				# kill all process with vlc in it
+				check_kill_process("vlc")
+				
+				# assemble command
+				command = "cvlc -I rc --rc-host localhost:4212 %s" % (playlist_file)
+				
+				logging.info("Command: %s", command )
+				# set VLC to new playlist
+				pid = subprocess.Popen(command,  shell=True,stdin=None, stdout=None, stderr=None, close_fds=True)
+				logging.info("Started process: %d", pid.pid )
+			else:
+				#
+				logging.warning("No files in %s.", music_folder )
+			#endif 
 			
-			# kill all process with vlc in it
-			check_kill_process("vlc")
-			
-			# assemble command
-			command = "cvlc -I rc --rc-host localhost:4212 %s" % (playlist_file)
-			
-			logging.info("Command: %s", command )
-			# set VLC to new playlist
-			pid = subprocess.Popen(command,  shell=True,stdin=None, stdout=None, stderr=None, close_fds=True)
-			logging.info("Started process: %d", pid.pid )
 		else:
-			#
-			logging.warning("No files in %s.", music_folder )
-		#endif 
-		
-	else:
-		# folder does not exists
-		logging.warning("No Folder for Card ID %s : %s", cardid, music_folder )
-	#endif
+			# folder does not exists
+			logging.warning("No Folder for Card ID %s : %s", cardid, music_folder )
+		#endif
 
-#end while
+	#end while
+else:
+	if reader.deviceName :
+		logging.error('Could not find the device %s\n. Make sure is connected' % deviceName)		
+	else:
+		# file is created by using script RegisterDevice.py
+		logging.error("No Device configured. Execute RegisterDevice.py")
