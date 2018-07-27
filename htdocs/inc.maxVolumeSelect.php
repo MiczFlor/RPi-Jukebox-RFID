6be92a6687d5d9953dfbd5c3f2ea8765107c1301
Joe <joe@Joes-MacBook-Air.local>
2018-07-27 23:51:46 +0200

    preselect actual settingvalues in volume setting form

--- htdocs/inc.maxVolumeSelect.php
+++ htdocs/inc.maxVolumeSelect.php
@@ -1,19 +1,30 @@
+<?php
+// get current maxvolumevalue
+	$maxvolumevalue = exec("/usr/bin/sudo ".$conf['scripts_abs']."/playout_controls.sh -c=getmaxvolume");
+?>
+
 <div class="row">
 	<div class="col-xs-3">
 		<div class="form-group">
 		<form name='maxvolume' method='post' action='<?php print $_SERVER['PHP_SELF']; ?>'>
 			<select name='maxvolume' id="sel2" class="form-control">
-				<option value='100'>100%</option>
-				<option value='90'>90%</option>
-				<option value='80'>80%</option>
-				<option value='70'>70%</option>
-				<option value='60'>60%</option>
-				<option value='50'>50%</option>
-				<option value='40'>40%</option>
-				<option value='30'>30%</option>
-				<option value='20'>20%</option>
-				<option value='10'>10%</option>
-				<option value='0'>Mute (0%)</option>
+			<?php   // make 10 steps from 100 to 0
+					for ($i=100;$i>=0; $i-=10)
+					{
+						// change the text on value 0 
+						if($i == 0){$step = "Mute (0%)";}
+						else {$step = $i."%";}
+						// check if item fits the actual value
+						if($maxvolumevalue == $i )
+						{
+							print "<option value='".$i."' selected='selected'>".$step."</option>";
+						}
+						else
+						{
+							print "<option value='".$i."'>".$step."</option>";
+						}
+					}		
+			?>
 			</select>
 		</div><!-- /.form-group -->
 	</div><!-- /.col-xs-3 -->
@@ -25,7 +36,6 @@
 	<div class="col-xs-7">
 		<div class="progress">
 			<?php
-			$maxvolumevalue = exec("/usr/bin/sudo ".$conf['scripts_abs']."/playout_controls.sh -c=getmaxvolume");
 			if ($maxvolumevalue == 0) {
 				print '<div class="progress-bar progress-bar progress-bar-danger" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">';
     				print 'Muted (0%)';
