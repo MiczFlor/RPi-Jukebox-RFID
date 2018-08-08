3bb9a08e0a1810d9ce8c8b09b3a6956c4b7021f1
zxa <jan.simon@gmx.de>
2018-08-09 01:25:24 +0200

    Enable play from playlist position

--- scripts/resume_play.sh
+++ scripts/resume_play.sh
@@ -5,7 +5,7 @@
 # audio is stored.
 # Usage: 
 # Save the position: ./resume_play.sh -c=savepos
-# Restore position and play: ./resume_play-sh -c=resume
+# Restore position and play or play from playlist position: ./resume_play-sh -c=resume -v=playlist_pos
 # Enable resume for folder: ./resume_play-sh -c=enableresume -v=foldername_in_audiofolders
 # Disable resume for folder: ./resume_play-sh -c=disableresume -v=foldername_in_audiofolders
 #
@@ -59,7 +59,7 @@ savepos)
             sudo sed -i 's/%PLAYSTATUS%/Stopped/' "$PATHDATA/../shared/audiofolders/$FOLDER/lastplayed.dat"
         fi
     fi
-;;
+    ;;
 resume)
     # Check if "lastplayed.dat" exists
     if [ -e "$PATHDATA/../shared/audiofolders/$FOLDER/lastplayed.dat" ];
@@ -74,23 +74,24 @@ resume)
         
         # Check if we got a "savepos" command after the last "resume". Otherwise we assume that the playlist was played until the end.
         # In this case, start the playlist from beginning 
-        if [ $PLAYSTATUS != "Playing" ]
+        if [ $PLAYSTATUS == "Stopped" ] 
         then
             # Get the playlist position of the file from mpd
             # Alternative approach: "mpc searchplay xx && mpc seek yy" 
             PLAYLISTPOS=$(echo -e playlistfind filename \"$FILENAME\"\\nclose | nc -w 1 localhost 6600 | grep -o -P '(?<=Pos: ).*')
 
-            # If the file is found, it is played from timestamp, otherwise start playlist from beginning        
-            if [ ! -z $PLAYLISTPOS ];
+            # If the file is found, it is played from timestamp, otherwise start playlist from beginning. If we got a playlist position
+            # play from that position, not the saved one.
+            if [ ! -z $PLAYLISTPOS ] && [ -z $VALUE ] ;
             then
                 echo -e seek $PLAYLISTPOS $TIMESTAMP \\nclose | nc -w 1 localhost 6600
             else
-                mpc play
+                echo -e "play $VALUE" | nc -w 1 localhost 6600
             fi
             # If the playlist ends without any stop/shutdown/new swipe (you've listened to all of the tracks), there's no savepos event
             # and we would resume at the last position anywhere in the playlist. To catch these, we signal it to the next "resume" call
             # via writing it to the lastplayed.dat that we still assume that the audio is playing. Remark: $FILENAME and $TIMESTAMP can
-            # be anything here, as we won't the information if "Playing" is found by "resume".
+            # be anything here, as we won't use the information if "Playing" is found by "resume".
             #printf "$FILENAME\n$TIMESTAMP\nPlaying" > "$PATHDATA/../shared/audiofolders/$FOLDER/lastplayed.dat.old"
             # copy sample file to audiofolder
             cp "$PATHDATA/../misc/lastplayed.dat.sample" "$PATHDATA/../shared/audiofolders/$FOLDER/lastplayed.dat"
@@ -101,13 +102,14 @@ resume)
             sudo sed -i 's/%PLAYSTATUS%/Playing/' "$PATHDATA/../shared/audiofolders/$FOLDER/lastplayed.dat"
         else
             # We assume that the playlist ran to the end the last time and start from the beginning.
-            mpc play
+            # Or: playlist is playing and we've got a play from playlist position command.
+            echo -e "play $VALUE" | nc -w 1 localhost 6600
         fi
     else
-        # if no lastplayed.dat exists, we play the playlist from the beginning
-        mpc play
+        # if no lastplayed.dat exists (resume play disabled), we play the playlist from the beginning or the given playlist position
+        echo -e "play $VALUE" | nc -w 1 localhost 6600
     fi
-;;
+    ;;
 enableresume)
     #echo -e "filename\n0\nStopped" > "$PATHDATA/../shared/audiofolders/$VALUE/lastplayed.dat.old"
     # copy sample file to audiofolder
@@ -116,12 +118,12 @@ enableresume)
     sudo sed -i 's/%FILENAME%/filename/' "$PATHDATA/../shared/audiofolders/$VALUE/lastplayed.dat"
     sudo sed -i 's/%TIMESTAMP%/0/' "$PATHDATA/../shared/audiofolders/$VALUE/lastplayed.dat"
     sudo sed -i 's/%PLAYSTATUS%/Stopped/' "$PATHDATA/../shared/audiofolders/$VALUE/lastplayed.dat"
-;;
+    ;;
 disableresume)
     #rm "$PATHDATA/../shared/audiofolders/$VALUE/lastplayed.dat.old"
     rm "$PATHDATA/../shared/audiofolders/$VALUE/lastplayed.dat"
-;;
+    ;;
 *)
     echo "Command unknown"
-;;
+    ;;
 esac
