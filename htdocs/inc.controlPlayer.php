6443371dee816da0b9fff824663caa1af203582e
Micz Flor <micz.flor@web.de>
2018-08-21 22:23:06 +0200

    seek icons web app

--- htdocs/inc.controlPlayer.php
+++ htdocs/inc.controlPlayer.php
@@ -1,14 +1,16 @@
 
 
 <?php
+/*
 if(file_exists($conf['base_path'].'/shared/audiofolders/Suli Pushban/cover.jpg')) { 
     print '<div class="playerWrapperCover"><center>';
     print '<img class="img-responsive img-thumbnail" src="image.php?img='.$conf['base_path'].'/shared/audiofolders/Suli Pushban/cover.jpg" alt="Suli Pushban"/>';
     print '</center></div>';
 }
+*/
 ?>
 <div class="playerWrapper">
-    <!--a href='?player=skipBack15'><img width=20 src='_assets/img/skip-15-back-grey.svg'/></a-->
+    <!--a href='?player=seekBack15'><img width=20 src='_assets/img/seek-15-back-grey.svg'/></a-->
     <a href='?player=prev' class='btn btn-player-l '><i class='fa fa-backward'></i></a>
 
 <?php
@@ -22,7 +24,7 @@ if(file_exists($conf['base_path'].'/shared/audiofolders/Suli Pushban/cover.jpg')
           <!--a href='?player=replay' class='btn btn-default btn-success btn-lg'><i class='fa fa-refresh'></i></a-->
 
     <a href='?player=next' class='btn btn-player-l '><i class='fa fa-forward'></i></a>
-    <!--a href='?player=skipAhead15'><img width=20 src='_assets/img/skip-15-forward-grey.svg'/></a-->
+    <!--a href='?player=seekForward15'><img width=20 src='_assets/img/seek-15-forward-grey.svg'/></a-->
 </div><!-- ./playerWrapper -->
 <div class="playerWrapper">
         <div class="btn-group controlVolumeUpDown" role="group" aria-label="volume" style="margin-bottom:0.5em;">
