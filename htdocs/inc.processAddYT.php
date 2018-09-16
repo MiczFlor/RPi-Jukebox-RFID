6809c00433751fe710f6c620b437d00d53e11c8f
emhal <emhalknarf@gmail.com>
2018-09-16 21:15:04 +0200

    Update inc.processAddYT.php

--- htdocs/inc.processAddYT.php
+++ htdocs/inc.processAddYT.php
@@ -5,7 +5,7 @@ if(isset($post['streamFolderName'])) {
     exec($exec);
     $foldername = $Audio_Folders_Path."/".$post['streamFolderName'];
     // New folder is created so we link a RFID to it. Write $post['streamFolderName'] to cardID file in shortcuts
-    $exec = "rm ".$fileshortcuts."; echo '".$foldername."' > ".$fileshortcuts."; chmod 777 ".$fileshortcuts;
+    $exec = "rm ".$fileshortcuts."; echo '".$post['streamFolderName']."' > ".$fileshortcuts."; chmod 777 ".$fileshortcuts;
     exec($exec);
 } else {
     // link to existing audiofolder
