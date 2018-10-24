1143cb21ae384d066c3229bab3425c34c5e662fb
Maxwell <37151645+princemaxwell@users.noreply.github.com>
2018-10-24 22:53:43 +0200

    Update ajax.loadMopidyStatus.php
    Introducing Phoniebox "Editions"... Phoniebox classic / Phoniebox +Spotify

--- htdocs/ajax.loadMopidyStatus.php
+++ htdocs/ajax.loadMopidyStatus.php
@@ -1,4 +1,4 @@
 <?php
-$mopidystatus = exec("sudo systemctl status mopidy |grep 'active '|sed 's/Active: //g'");
+$mopidystatus = exec("sudo systemctl status mopidy |grep 'Active: '|sed 's/Active: //g'");
 ?>
           <div class="col-md-6"><?php echo trim($mopidystatus); ?></div>
