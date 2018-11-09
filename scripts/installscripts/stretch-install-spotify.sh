a564d739179aca20680f0825cb50fc8f41aaa74a
Micz Flor <micz.flor@web.de>
2018-11-09 13:57:54 +0100

    install script apt-get python3-gpiozero

--- scripts/installscripts/stretch-install-spotify.sh
+++ scripts/installscripts/stretch-install-spotify.sh
@@ -456,7 +456,7 @@ sudo iwconfig wlan0 power off
 
 # Install required packages
 sudo apt-get update
-sudo apt-get install apt-transport-https samba samba-common-bin python-dev python-pip gcc linux-headers-4.9 lighttpd php7.0-common php7.0-cgi php7.0 php7.0-fpm at mpd mpc mpg123 git ffmpeg python-mutagen
+sudo apt-get install apt-transport-https samba samba-common-bin python-dev python-pip gcc linux-headers-4.9 lighttpd php7.0-common php7.0-cgi php7.0 php7.0-fpm at mpd mpc mpg123 git ffmpeg python-mutagen python3-gpiozero
 sudo pip install "evdev == 0.7.0"
 sudo pip install youtube_dl
 
