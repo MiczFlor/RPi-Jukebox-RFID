fb084324d738a0f35b6a27225fa0de97aa82acbf
Micz Flor <micz.flor@web.de>
2018-12-26 13:53:29 +0100

    reader.py fixes and alternative scripts in comments #407

--- scripts/Reader.py
+++ scripts/Reader.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python2
-# If you encounter problems with this 1.1.9 and newer version of the script Reader.py
-# you can try the script Reader.py.backupVersion1.1.7 which is the prior version.
-# The older version Reader.py.backupVersion1.1.7 created some problems with 
-# KKMOON RFID Reader which appears twice in the devices list as HID 413d:2107
-# and this required to check "if" the device is a keyboard.
+# There are a variety of RFID readers out there, USB and non-USB variants.
+# This might create problems in recognizing the reader you are using.
+# We haven't found the silver bullet yet. If you can contribute to this 
+# quest, please comment in the issue thread or create pull requests.
+# ALTERNATIVE SCRIPTS:
+# If you encounter problems with this script Reader.py
+# consider and test one of the alternatives in the same scripts folder.
+# Replace the Reader.py file with one of the following files:
+# * Reader.py.experimental
+#     This alternative Reader.py script was meant to cover not only USB readers but more.
+#     It can be used to replace Reader.py if you have readers such as
+#     MFRC522 or RDM6300
+# * Reader.py.kkmoonRFIDreader
+#     KKMOON RFID Reader which appears twice in the devices list as HID 413d:2107
+#     and this required to check "if" the device is a keyboard.
 
 import string
 #import csv
@@ -12,29 +22,25 @@ import sys
 
 from evdev import InputDevice, categorize, ecodes, list_devices
 from select import select
+
+def get_devices():
+    return [InputDevice(fn) for fn in list_devices()]
+
 class Reader:
-
-	def is_Keyboard(self, device):
-		device_key_list = device.capabilities()[ecodes.EV_KEY]
-
-		if self.mandatory_keys.issubset(device_key_list) and self.reserved_key.isdisjoint(device_key_list):
-			return True
-		else:
-			return False
+    reader = None
 
 	def __init__(self):
+	    self.reader = self
 		path = os.path.dirname(os.path.realpath(__file__))
 		self.keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
-		self.mandatory_keys = {i for i in range(ecodes.KEY_ESC, ecodes.KEY_D)}
-		self.reserved_key = {0}
 		if not os.path.isfile(path + '/deviceName.txt'):
 			sys.exit('Please run RegisterDevice.py first')
 		else: 
 			with open(path + '/deviceName.txt','r') as f:
 				deviceName = f.read()
-			devices = [InputDevice(fn) for fn in list_devices()]
+			devices = get_devices()
 			for device in devices:
-				if device.name == deviceName and self.is_Keyboard(device):
+				if device.name == deviceName:
 					self.dev = device
 					break 		
 			try:
@@ -53,3 +59,4 @@ class Reader:
 				#print( keys[ event.code ] )
 				key = ecodes.KEY[ event.code ]
 		return stri[:-1]
+		
