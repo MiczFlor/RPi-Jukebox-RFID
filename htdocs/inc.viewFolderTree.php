fd419884002397423a1834cc41ba04e6099817d6
Micz Flor <micz.flor@web.de>
2019-02-03 16:32:06 +0100

    expand folders in web ui with [] chars #469

--- htdocs/inc.viewFolderTree.php
+++ htdocs/inc.viewFolderTree.php
@@ -126,6 +126,8 @@ foreach($subfolders as $key => $subfolder) {
         // some special version with no slashes or whitespaces for IDs on the panel collapse
         $temp['id'] = preg_replace('/\//', '---', $temp['path_rel']);
         $temp['id'] = preg_replace('/\ /', '-_-', $temp['id']);
+        $temp['id'] = preg_replace('/[/', '-\[-', $temp['id']);
+        $temp['id'] = preg_replace('/]/', '-\]-', $temp['id']);
         $temp['id'] = "ID".preg_replace('/\:/', '-+-', $temp['id']);
         // count the level depth in the tree by counting the slashes in the path
         $temp['level'] = substr_count($temp['path_rel'], '/');
