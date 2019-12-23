ed3c031ca8f7cebe6295a9b1519d92e31a728c6a
ctietze <clemens.tietze@gmx.de>
2019-12-24 00:39:08 +0100

    Fix edit track information #674
    - Quote filename to handle files and folders /w whitespaces
    - Change the way of adding mid3v2 cli options for artist, song, album and composer according to
    https://mutagen.readthedocs.io/en/latest/man/mid3v2.html

--- tests/htdocs/trackEditTest.php
+++ tests/htdocs/trackEditTest.php
@@ -12,7 +12,7 @@ class TrackEditTest extends TestCase {
     use PHPMock;
 
     const CONFIG_FILENAME = "config.php";
-    const FILENAME = "file to move üöä";
+    const FILENAME = "file to move üöä.mp3";
     const SOURCE_FOLDER_NAME = "source_folder";
     const TARGET_FOLDER_NAME = "target_folder";
     private $sourceFolder;
@@ -37,24 +37,12 @@ class TrackEditTest extends TestCase {
         fopen($this->sourceFolder . DIRECTORY_SEPARATOR . self::FILENAME, "w");
         fopen(self::CONFIG_FILENAME, "w");
         $_SERVER['HTTP_HOST'] = 'localhost';
-    }
 
-    public function tearDown() : void {
-        unlink($this->targetFolder . DIRECTORY_SEPARATOR .  self::FILENAME);
-        unlink(self::CONFIG_FILENAME);
-
-        if (is_dir($this->sourceFolder)) {
-            rmdir($this->sourceFolder);
-        }
-
-        if(is_dir($this->targetFolder)) {
-            rmdir($this->targetFolder);
-        }
-    }
-
-    public function testMoveTrack() {
         $file_exists = $this->getFunctionMock(__NAMESPACE__, 'file_exists');
         $file_exists->expects($this->atLeastOnce())->willReturn(true);
+
+        $shell_exec = $this->getFunctionMock(__NAMESPACE__, 'shell_exec');
+        $shell_exec->expects($this->atLeastOnce())->willReturn(true);
 
         $file_get_contents = $this->getFunctionMock(__NAMESPACE__, 'file_get_contents');
         $file_get_contents->expects($this->atLeastOnce())->willReturn(sys_get_temp_dir());
@@ -62,17 +50,40 @@ class TrackEditTest extends TestCase {
         $parse_ini_file = $this->getFunctionMock(__NAMESPACE__, 'parse_ini_file');
         $parse_ini_file->expects($this->atLeastOnce())->willReturn(
             array(
-            "DEBUG_WebApp" => "FALSE",
-            "DEBUG_WebApp_API" => "FALSE",
-            "SECONDSWIPE" => "FALSE",
-            "SHOWCOVER" => "FALSE",
-            "VERSION" => "FALSE",
-            "EDITION" => "FALSE",
-            "AUDIOVOLMAXLIMIT" => "FALSE",
-            "LANG" => "FALSE",
-            "AUDIOFOLDERSPATH" => sys_get_temp_dir()
-        ));
+                "DEBUG_WebApp" => "FALSE",
+                "DEBUG_WebApp_API" => "FALSE",
+                "SECONDSWIPE" => "FALSE",
+                "SHOWCOVER" => "FALSE",
+                "VERSION" => "FALSE",
+                "EDITION" => "FALSE",
+                "AUDIOVOLMAXLIMIT" => "FALSE",
+                "LANG" => "FALSE",
+                "AUDIOFOLDERSPATH" => sys_get_temp_dir()
+            ));
+    }
 
+    public function tearDown() : void {
+        unlink(self::CONFIG_FILENAME);
+
+        if (is_dir($this->sourceFolder)) {
+            if (is_file($this->sourceFolder . DIRECTORY_SEPARATOR .  self::FILENAME)) {
+                unlink($this->sourceFolder . DIRECTORY_SEPARATOR .  self::FILENAME);
+            }
+            rmdir($this->sourceFolder);
+        }
+
+        if(is_dir($this->targetFolder)) {
+            if (is_file($this->targetFolder . DIRECTORY_SEPARATOR .  self::FILENAME)) {
+                unlink($this->targetFolder . DIRECTORY_SEPARATOR .  self::FILENAME);
+            }
+            rmdir($this->targetFolder);
+        }
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testMoveTrack() {
         $_POST['ACTION'] = "trackMove";
         $_POST['filename'] = TrackEditTest::FILENAME;
         $_POST['folderNew'] = Files::buildPath(sys_get_temp_dir(), TrackEditTest::TARGET_FOLDER_NAME);
@@ -82,5 +93,34 @@ class TrackEditTest extends TestCase {
         require_once 'htdocs/trackEdit.php';
         $this->assertEquals($_POST['folderNew'], $post['folder']);
         $this->assertTrue(is_file($_POST['folderNew'] . DIRECTORY_SEPARATOR . TrackEditTest::FILENAME));
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testUpdateTrack() {
+        $_POST['ACTION'] = "trackUpdate";
+        $_POST['folder'] = Files::buildPath(sys_get_temp_dir(), TrackEditTest::SOURCE_FOLDER_NAME);
+        $_POST['filename'] = TrackEditTest::FILENAME;
+        $conf['base_url']= "localhost";
+        $post['trackArtist'] = "Lorem ipsum";
+        $post['trackTitle'] = "dolor sit";
+        $post['trackAlbum'] = "amet consectetur";
+        $post['trackComposer'] = "adipisici elit äüö";
+
+        $exec = $this->getFunctionMock(__NAMESPACE__, 'exec');
+        $exec->expects($this->atLeastOnce())->willReturnCallback(
+            function ($command) {
+                $mid3v2 = "mid3v2 "
+                    . "--artist='Lorem ipsum' "
+                    . "--album='amet consectetur' "
+                    . "--song='dolor sit' "
+                    . "--TCOM='adipisici elit aeueoe' '/tmp/source_folder/file to move üöä.mp3'";
+                $this->assertEquals(addslashes($mid3v2), addslashes($command));
+
+            }
+        );
+
+        require_once 'htdocs/trackEdit.php';
     }
 }
