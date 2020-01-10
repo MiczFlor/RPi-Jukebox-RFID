4bd7649d91c9d97b6d1d29734e8bc99a72bbd199
Patrick Weigelt <pweigel@uni-goettingen.de>
2020-01-10 22:31:05 +0100

    back again

--- scripts/installscripts/buster-install-default.sh
+++ scripts/installscripts/buster-install-default.sh
@@ -463,14 +463,9 @@ esac
 # power management of wifi: switch off to avoid disconnecting
 sudo iwconfig wlan0 power off
 
-# Install libspotify-dev
-wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
-sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list
-sudo apt-get update
-
-sudo apt-get install libspotify-dev
 
 # Install required packages
+sudo apt-get update
 sudo apt-get --yes --allow-downgrades --allow-remove-essential --allow-change-held-packages install apt-transport-https samba samba-common-bin python-dev python-pip gcc raspberrypi-kernel-headers lighttpd php7.3-common php7.3-cgi php7.3 php7.3-fpm at mpd mpc mpg123 git ffmpeg python-mutagen python3-gpiozero resolvconf spi-tools python-spidev python3-spidev
 
 # Install required spotify packages
