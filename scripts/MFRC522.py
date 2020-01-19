f881e01b1a4eda1a718e7daab41e32de8f1f402e
s-martin <s-martin@users.noreply.github.com>
2020-01-19 10:09:44 +0100

    Fixed unintentional brace

--- scripts/MFRC522.py
+++ scripts/MFRC522.py
@@ -340,7 +340,7 @@ class MFRC522:
       print("Error while reading!")
     i = 0
     if len(backData) == 16:
-      print()"Sector "+str(blockAddr)+" "+str(backData))
+      print("Sector "+str(blockAddr)+" "+str(backData))
   
   def MFRC522_Write(self, blockAddr, writeData):
     buff = []
