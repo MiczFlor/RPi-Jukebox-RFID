a2761c5d05017021904f4e267885964cbfc22cbc
Felix bachmair <felix.bachmair@gmail.com>
2020-04-17 07:40:41 +0200

    update for better error handing

--- components/gpio_control/install.sh
+++ components/gpio_control/install.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
+if [[ $(id -u) = 0 ]]; then
+   echo "This script should  be run as root/sudo, please run as normal 'pi' user" 
+   exit 1
+fi
+
 echo 'Install all required python modules'
 pip install -r requirements.txt
 
 echo 'Installing GPIO_Control service'
 echo
 
-if [!-d $HOME/.config/phoniebox/]; then
+CONFIG_PATH=$HOME/.config/phoniebox
+if [ ! -d $CONFIG_PATH ]; then
     mkdir -p $HOME/.config/phoniebox/ ;
 fi;
 
-FILE=$HOME/.config/phoniebox/gpio_settings.ini
+FILE=$CONFIG_PATH/gpio_settings.ini
 if test -f "$FILE"; then
     echo "$FILE exist"
     echo "Script will not install a configuration"
@@ -46,7 +53,10 @@ read -p "Press enter to continue " -n 1 -r
 SERVICE_FILE=/etc/systemd/system/phoniebox_gpio_control.service
 if test -f "$SERVICE_FILE"; then
    echo "$SERVICE_FILE exists.";
+   echo 'restarting service'
+   systemctl restart phoniebox_gpio_control.service
    read -p "Press enter to continue " -n 1 -r;
+
     #echo "systemctl daemon-reload"
     #systemctl daemon-reload
 else
@@ -60,16 +70,21 @@ SERVICE_STATUS="$(systemctl is-active phoniebox_gpio_control.service)"
 if [ "${SERVICE_STATUS}" = "active" ]; then
     echo "Phoniebox GPIO Service started correctly ....."
 else
-    echo " Service not running, please check functionallity by running gpio_control.py "
-    echo " in the directory ~/RPi-Jukebox-RFID/components: "
-    echo "$ cd ~/RPi-Jukebox-RFID/components"
-    echo "$ python gpio_control.py"
-    echo " or check output of journaclctl by:"
-    echo "$ journalctl -u phoniebox_gpio_control.service -f"
+    echo ""
+    FRED="\033[31m"
+    FBOLD='\033[1;31m'
+    RS="\033[0m"
+    echo -e "$FRED"$FBOLD"Problem during installation occured $RS"
+    echo "   Service not running, please check functionallity by running gpio_control.py "
+    echo "   in the directory ~/RPi-Jukebox-RFID/components/gpio_control: "
+    echo "      $ cd ~/RPi-Jukebox-RFID/components/gpio_control"
+    echo "      $ python gpio_control.py"
+    echo "   or check output of journaclctl by:"
+    echo "      $ journalctl -u phoniebox_gpio_control.service -f"
     exit 1
 fi
-systemctl is-active --quiet phoniebox_gpio_control.service
-systemctl status phoniebox_gpio_control.service
+#systemctl is-active --quiet phoniebox_gpio_control.service
+#systemctl status phoniebox_gpio_control.service
 
 
 
