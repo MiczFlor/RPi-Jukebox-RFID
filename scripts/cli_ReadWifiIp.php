0a93e866bbfc84d53cfde28321ff3b42b5e43ba6
Micz Flor <micz.flor@web.de>
2020-05-02 15:58:18 +0200

    #921 read out local wifi IP and fix some bugs

--- scripts/cli_ReadWifiIp.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-$wifiIp = exec("ifconfig wlan0 | grep \"inet \" | awk -F'[: ]+' '{ print $3 }'");
-print $wifiIp;
-$array = str_split($wifiIp);
-foreach ($array as $char) {
- echo $char."-";
-}
-?>
