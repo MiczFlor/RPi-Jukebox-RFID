abff64fc3cb1bb1319f57960158a96aa4efa38f6
s-martin <saimen54@hotmail.com>
2020-05-10 12:11:22 +0200

    fixed paths

--- scripts/installscripts/tests/run_installation_tests.sh
+++ scripts/installscripts/tests/run_installation_tests.sh
@@ -3,13 +3,11 @@
 # Install Phoniebox and test it
 # Used e.g. for tests on Docker
 
-# TODO path code for Docker to be removed
-
 # print current path
 echo $PWD
 
 # run installation
-./code/scripts/installscripts/buster-install-default.sh
+./scripts/installscripts/buster-install-default.sh
 
 # test installation
-./code/scripts/installscripts/tests/test_installation.sh
+./scripts/installscripts/tests/test_installation.sh
