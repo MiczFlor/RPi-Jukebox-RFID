c73a5e7ea1160044c377e7068a31d7e8e294402d
s-martin <saimen54@hotmail.com>
2020-05-10 11:54:33 +0200

    fix test of installation script on docker

---
+++ scripts/installscripts/tests/run_installation_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Phoniebox and test it
+# Used e.g. for tests on Docker
+
+# run installation
+../buster-install-default.sh
+
+# test installation
+./test_installation.sh
