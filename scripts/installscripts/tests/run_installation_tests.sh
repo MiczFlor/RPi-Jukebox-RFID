07e70ca8d2dc91f5b82886ec8d4861fd0c1a37a8
s-martin <saimen54@hotmail.com>
2020-05-10 12:31:26 +0200

    send interactive commands

--- scripts/installscripts/tests/run_installation_tests.sh
+++ scripts/installscripts/tests/run_installation_tests.sh
@@ -6,8 +6,15 @@
 # print current path
 echo $PWD
 
-# run installation
-./scripts/installscripts/buster-install-default.sh
+# run installation (in interactive mode)
+# y confirm interactive
+# n dont configure wifi
+# y PCM as iface
+# n no spotify
+# y configure mpd
+# y audio default location
+# y start installation
+./scripts/installscripts/buster-install-default.sh <<< $'y\nn\ny\nn\ny\ny\ny\n'
 
 # test installation
 ./scripts/installscripts/tests/test_installation.sh
