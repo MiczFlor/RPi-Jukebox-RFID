379cb241aece2cb762a1080803a80ebc0424d45d
s-martin <saimen54@hotmail.com>
2020-05-10 13:30:12 +0200

    fix commands to interactive script

--- scripts/installscripts/tests/run_installation_tests.sh
+++ scripts/installscripts/tests/run_installation_tests.sh
@@ -9,6 +9,8 @@ echo $PWD
 # Preparations
 # Skip interactive Samba WINS config dialog
 echo "samba-common samba-common/dhcp boolean false" | sudo debconf-set-selections
+# No interactive frontend
+export DEBIAN_FRONTEND=noninteractive
 
 # Run installation (in interactive mode)
 # y confirm interactive
@@ -17,8 +19,9 @@ echo "samba-common samba-common/dhcp boolean false" | sudo debconf-set-selection
 # n no spotify
 # y configure mpd
 # y audio default location
-# y start installation
-./scripts/installscripts/buster-install-default.sh <<< $'y\nn\ny\nn\ny\ny\ny\n'
+# n no RFID registration
+# n No reboot
+./scripts/installscripts/buster-install-default.sh <<< $'y\nn\ny\nn\ny\ny\nn\nn\n'
 
 # Rest installation
 ./scripts/installscripts/tests/test_installation.sh
