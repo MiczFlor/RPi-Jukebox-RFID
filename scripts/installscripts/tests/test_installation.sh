c3dabe654465db5a25ba5ce1e9c7405d8477be65
s-martin <saimen54@hotmail.com>
2020-05-17 21:53:25 +0200

    workaround for apt-transport-https

--- scripts/installscripts/tests/test_installation.sh
+++ scripts/installscripts/tests/test_installation.sh
@@ -152,11 +152,12 @@ verify_wifi_settings() {
 }
 
 verify_apt_packages(){
-    local packages="apt-transport-https libspotify-dev samba
+    local packages="libspotify-dev samba
 samba-common-bin gcc lighttpd php7.3-common php7.3-cgi php7.3 at mpd mpc mpg123 git ffmpeg
 resolvconf spi-tools python3 python3-dev python3-pip python3-mutagen python3-gpiozero
 python3-spidev"
-    local packages_raspberrypi="raspberrypi-kernel-headers"
+    # TODO apt-transport-https checking only on RPi is currently a workaround
+    local packages_raspberrypi="apt-transport-https raspberrypi-kernel-headers"
     local packages_spotify="mopidy mopidy-mpd mopidy-local mopidy-spotify libspotify12
 python3-cffi python3-ply python3-pycparser python3-spotify"
 
@@ -169,7 +170,7 @@ python3-cffi python3-ply python3-pycparser python3-spotify"
 
     # check for raspberry pi packages only on raspberry pi's but not on test docker containers running on x86_64 machines
     if [[ $(uname -m) =~ ^armv.+$ ]]; then
-        packages="${packages} ${packages_raspbrrypi}"
+        packages="${packages} ${packages_raspberrypi}"
     fi
 
     for package in ${packages}
