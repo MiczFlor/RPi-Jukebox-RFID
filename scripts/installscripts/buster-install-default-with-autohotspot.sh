03e63de6669c49548c9246185f36554f245f7347
RalfAlbers <44479345+RalfAlbers@users.noreply.github.com>
2020-05-30 14:17:40 +0200

    Update buster-install-default-with-autohotspot.sh (#984)
    fix missing variable
    set it local inside function

--- scripts/installscripts/buster-install-default-with-autohotspot.sh
+++ scripts/installscripts/buster-install-default-with-autohotspot.sh
@@ -1037,6 +1037,7 @@ folder_access() {
 
 autohotspot() {
     local jukebox_dir="$1"
+    local apt_get="sudo apt-get -qq --yes"
 
     # adapted from https://www.raspberryconnect.com/projects/65-raspberrypi-hotspot-accesspoints/158-raspberry-pi-auto-wifi-hotspot-switch-direct-connection
 
