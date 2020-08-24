5db674dfda9cf658104a9db5282c510b0512b2d7
themorlan <48086551+themorlan@users.noreply.github.com>
2020-08-24 14:29:41 +0200

    Update inc.setInputDevices.php
    Remove old GPIO-Services and add control for new service

--- htdocs/inc.setInputDevices.php
+++ htdocs/inc.setInputDevices.php
@@ -3,7 +3,7 @@
 	<div class="row">
 		<div class="col-xs-6">
 <?php
-$gpiostatus = exec("/bin/systemctl status phoniebox-gpio-buttons.service | grep running");
+$gpiostatus = exec("/bin/systemctl status phoniebox-gpio-control.service | grep running");
 if ($gpiostatus != "") {
 	print $lang['globalGpioButtons'].' <span class="label label-success">'.$lang['globalEnabled'].'</span>';
 	print '</div><!-- / .col-xs-6 -->';
@@ -15,28 +15,6 @@ else {
 	print '</div><!-- / .col-xs-4 -->';
 	print '<div class="col-xs-4">';
 	print "<a href='?gpiostatus=turnon' class='btn btn-sm btn-success'><i class='mdi mdi-play'></i> ".$lang['globalSwitchOn']."</a>";
-}
-?>
-		</div><!-- / .col-xs-6 -->
-	</div><!-- / .row -->
-  </li>
-
-  <li class="list-group-item">
-	<div class="row">
-		<div class="col-xs-6">
-<?php
-$rotarystatus = exec("/bin/systemctl status phoniebox-rotary-encoder.service | grep running");
-if ($rotarystatus != "") {
-	print $lang['globalRotaryKnob'].' <span class="label label-success">'.$lang['globalEnabled'].'</span>';
-	print '</div><!-- / .col-xs-6 -->';
-	print '<div class="col-xs-6">';
-	print "<a href='?rotarystatus=turnoff' class='btn btn-sm btn-danger'><i class='mdi mdi-cancel'></i> ".$lang['globalSwitchOff']."</a>";
-}
-else {
-	print $lang['globalRotaryKnob'].' <span class="label label-danger">'.$lang['globalDisabled'].'</span>';
-	print '</div><!-- / .col-xs-4 -->';
-	print '<div class="col-xs-4">';
-	print "<a href='?rotarystatus=turnon' class='btn btn-sm btn-success'><i class='mdi mdi-play'></i> ".$lang['globalSwitchOn']."</a>";
 }
 ?>
 		</div><!-- / .col-xs-6 -->
