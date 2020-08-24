bd2c345838796e5ad5e1a662e0f5f8ec57c4ec3d
themorlan <marlon@Marlons-MacBook-Air.local>
2020-08-24 16:57:23 +0200

    typo

--- components/gpio_control/check_installation.sh
+++ components/gpio_control/check_installation.sh
@@ -29,7 +29,7 @@ else
     echo "      $ cd ~/RPi-Jukebox-RFID/components/gpio_control"
     echo "      $ python gpio_control.py"
     echo "   or check output of journaclctl by:"
-    echo "      $ journalctl -u phoniebox_gpio_control.service -f"
+    echo "      $ journalctl -u phoniebox-gpio-control.service -f"
 fi
 echo
 if $ERROR ; then
