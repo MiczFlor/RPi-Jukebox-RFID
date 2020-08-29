52f749847c2c006f63689f6caca51dbb04018ccc
andreas <andreas.schroden@gmail.com>
2020-08-29 14:51:53 +0200

    improved code for playout_controls_chapter.php for better readability

--- scripts/playout_controls_chapter.php
+++ scripts/playout_controls_chapter.php
@@ -2,62 +2,116 @@
 ini_set('display_errors', 'on');
 error_reporting(E_ALL);
 
+use M4bTool\Audio\Chapter;
 use M4bTool\Executables\Mp4chaps;
 use Sandreas\Time\TimeUnit;
 
-$inputCommand = $argv[1] ?? "";
-$inputValue = $argv[2] ?? "";
-$inputChaptersFile = new SplFileInfo($argv[3] ?? "");
-$inputElapsedTime = $argv[4] ?? "0";
-
-$m4btoolPhar = realpath(__DIR__ . "/m4b-tool.phar");
-if (!$m4btoolPhar) {
-    echo $inputCommand . ";" . $inputValue;
-    exit;
-}
-require_once "phar://" . $m4btoolPhar . "/vendor/autoload.php";
-
-if (!in_array($inputCommand, ["playernext", "playerprev"]) || !$inputChaptersFile->isFile() || $inputChaptersFile->getSize() === 0) {
-    echo $inputCommand . ";" . $inputValue;
-    exit;
-}
-$inputElapsedTimeUnit = new TimeUnit((float)$inputElapsedTime, TimeUnit::SECOND);
-
-$mp4chaps = new Mp4chaps();
-$chapters = $mp4chaps->parseChaptersTxt(file_get_contents($inputChaptersFile));
-
 // prev button skips to previous chapter within 5 seconds from start of current chapter
 const PREV_TOLERANCE_MILLISECONDS = 5000;
+const COMMAND_PLAYER_SEEK = "playerseek";
+const COMMAND_PLAYER_NEXT = "playernext";
+const COMMAND_PLAYER_PREV = "playerprev";
 
-$firstChapter = reset($chapters);
-$lastChapter = end($chapters);
 
-$prevChapter = $firstChapter;
-$nextChapter = $lastChapter;
+echo implode(";", rewriteCommandValueIfRequired($argv));
 
-if ($inputElapsedTimeUnit->milliseconds() > $nextChapter->getEnd()->milliseconds() && $inputCommand === "playernext") {
-    echo $inputCommand . ";" . $inputValue;
-    exit;
-}
+function rewriteCommandValueIfRequired($scriptArguments)
+{
+    $command = $scriptArguments[1] ?? "";
+    $value = $scriptArguments[2] ?? "";
+    $chaptersFile = new SplFileInfo($scriptArguments[3] ?? "");
+    $elapsedTime = $scriptArguments[4] ?? "0";
 
-foreach ($chapters as $chapter) {
-    if ($chapter->getStart()->milliseconds() < $inputElapsedTimeUnit->milliseconds() - PREV_TOLERANCE_MILLISECONDS && $chapter->getStart()->milliseconds() >= $prevChapter->getEnd()->milliseconds()) {
-        $prevChapter = $chapter;
-    } else if ($chapter->getStart()->milliseconds() > $inputElapsedTimeUnit->milliseconds() && $chapter->getEnd()->milliseconds() < $nextChapter->getStart()->milliseconds()) {
-        $nextChapter = $chapter;
-        break;
+    $originalCommandValue = [$command, $value];
+
+    if (!includeM4bToolAutoload() || !shouldRewriteCommand($command, $chaptersFile)) {
+        return $originalCommandValue;
     }
+
+    $chapters = parseChapters($chaptersFile);
+    $elapsedTimeUnit = new TimeUnit((float)$elapsedTime, TimeUnit::SECOND);
+
+    if (isLastChapterAlreadyPlaying($chapters, $elapsedTimeUnit, $command)) {
+        return $originalCommandValue;
+    }
+
+
+    list($prevChapter, $nextChapter) = findPrevAndNextChapter($chapters, $elapsedTimeUnit);
+
+    if ($command === COMMAND_PLAYER_PREV && $prevChapter !== null) {
+        return rewriteCommand($prevChapter);
+    }
+
+    if ($command === COMMAND_PLAYER_NEXT && $nextChapter !== null) {
+        return rewriteCommand($nextChapter);
+    }
+
+    return [$command, $value];
 }
 
-if ($inputCommand === "playerprev" && $prevChapter !== $firstChapter) {
-    echo "playerseek;" . round($prevChapter->getStart()->milliseconds() / 1000, 3);
-    exit;
+function includeM4bToolAutoload()
+{
+    $m4btoolPhar = realpath(__DIR__ . "/m4b-tool.phar");
+    if (!$m4btoolPhar) {
+        return false;
+    }
+    require_once "phar://" . $m4btoolPhar . "/vendor/autoload.php";
+    return true;
 }
 
-if ($inputCommand === "playernext" && $nextChapter !== $lastChapter) {
-    echo "playerseek;" . round($nextChapter->getStart()->milliseconds() / 1000, 3);
-    exit;
+function shouldRewriteCommand($command, SplFileInfo $chaptersFile)
+{
+    return in_array($command, [COMMAND_PLAYER_NEXT, COMMAND_PLAYER_PREV]) && $chaptersFile->isFile() && $chaptersFile->getSize() > 0;
 }
 
-echo $inputCommand . ";" . $inputValue;
+function parseChapters(SplFileInfo $chaptersFile)
+{
+    $mp4chaps = new Mp4chaps();
+    return $mp4chaps->parseChaptersTxt(file_get_contents($chaptersFile));
+}
+
+function isLastChapterAlreadyPlaying($chapters, TimeUnit $elapsedTimeUnit, $command)
+{
+    if ($command !== COMMAND_PLAYER_NEXT) {
+        return false;
+    }
+    $lastChapter = end($chapters);
+    return $elapsedTimeUnit->milliseconds() > $lastChapter->getEnd()->milliseconds();
+}
+
+
+function findPrevAndNextChapter($chapters, TimeUnit $inputElapsedTimeUnit)
+{
+    $prevChapter = null;
+    $nextChapter = null;
+
+    foreach ($chapters as $chapter) {
+        if (isChapterBetterMatchForPrevChapter($chapter, $prevChapter, $inputElapsedTimeUnit)) {
+            $prevChapter = $chapter;
+        } else if (isChapterBetterMatchForNextChapter($chapter, $nextChapter, $inputElapsedTimeUnit)) {
+            $nextChapter = $chapter;
+            // if we found a better match next chapter, we can stop searching
+            break;
+        }
+    }
+    return [$prevChapter, $nextChapter];
+}
+
+function isChapterBetterMatchForPrevChapter(Chapter $chapter, Chapter $prevChapter = null, TimeUnit $inputElapsedTimeUnit = null)
+{
+    return $chapter->getStart()->milliseconds() < $inputElapsedTimeUnit->milliseconds() - PREV_TOLERANCE_MILLISECONDS && ($prevChapter === null || $chapter->getStart()->milliseconds() >= $prevChapter->getEnd()->milliseconds());
+}
+
+function isChapterBetterMatchForNextChapter(Chapter $chapter, Chapter $nextChapter = null, TimeUnit $inputElapsedTimeUnit = null)
+{
+    return $chapter->getStart()->milliseconds() > $inputElapsedTimeUnit->milliseconds() && ($nextChapter === null || $chapter->getEnd()->milliseconds() < $nextChapter->getStart()->milliseconds());
+}
+
+
+function rewriteCommand(Chapter $chapter)
+{
+    return [COMMAND_PLAYER_SEEK, round($chapter->getStart()->milliseconds() / 1000, 3)];
+}
+
+
 
