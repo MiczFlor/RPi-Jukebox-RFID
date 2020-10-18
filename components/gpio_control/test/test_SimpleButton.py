02a039d9261cd9ab3cdd3d1761daede719f9733d
Berdsen <berdsen.home@gmail.com>
2020-10-18 11:59:59 +0200

    fixed unit tests fixes #1091

--- components/gpio_control/test/test_SimpleButton.py
+++ components/gpio_control/test/test_SimpleButton.py
@@ -22,16 +22,35 @@ class TestButton:
                      bouncetime=500, edge=GPIO.FALLING)
 
     def test_callback(self, simple_button):
-        simple_button.callbackFunctionHandler()(simple_button.pin)
+        simple_button.callbackFunctionHandler(simple_button.pin)
         mockedAction.asser_called_once()
+        mockedAction.assert_called_with()
+
+    def test_callback_without_pin_argument(self, simple_button):
+        simple_button.callbackFunctionHandler()
+        mockedAction.asser_called_once()
+        mockedAction.assert_called_with()
+
+    def test_callback_with_wrong_pin_argument(self, simple_button):
+        simple_button.callbackFunctionHandler(simple_button.pin + 1)
+        mockedAction.asser_called_once()
+        mockedAction.assert_called_with(simple_button.pin + 1)
+
+    def test_callback_without_more_arguments(self, simple_button):
+        simple_button.callbackFunctionHandler(simple_button.pin, 5)
+        mockedAction.asser_called_once()
+        mockedAction.assert_called_with(5)
 
     def test_change_when_pressed(self, simple_button):
         mockedAction.asser_called_once()
         newMockedAction = MagicMock()
         simple_button.when_pressed = newMockedAction
-        simple_button.callbackFunctionHandler()(simple_button.pin)
+        simple_button.callbackFunctionHandler(simple_button.pin)
         newMockedAction.asser_called_once()
+        newMockedAction.assert_called_with()
         mockedAction.asser_called_once()
+        # from last test
+        mockedAction.assert_called_with(5)
 
     def test_hold(self, simple_button):
         GPIO.LOW = 0
