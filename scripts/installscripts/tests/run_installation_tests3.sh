ce76d5b7571fbcff8d9682a0523145267ade9c05
s-martin <saimen54@hotmail.com>
2020-10-20 23:19:32 +0200

    fix accidental ENTER

--- scripts/installscripts/tests/run_installation_tests3.sh
+++ scripts/installscripts/tests/run_installation_tests3.sh
@@ -26,7 +26,7 @@ export DEBIAN_FRONTEND=noninteractive
 # n No reboot
 
 # TODO check, how this behaves on branches other than develop
-GIT_BRANCH=develop bash ./scripts/installscripts/buster-install-default.sh <<< $'y\nn\n\ny\n\ny\n\nmyuser\nmypassword\nmyclient_id\nmyclient_secret\n\ny\n\ny\n\ny\n\ny\nn\nn\n'
+GIT_BRANCH=develop bash ./scripts/installscripts/buster-install-default.sh <<< $'y\nn\n\ny\n\ny\nmyuser\nmypassword\nmyclient_id\nmyclient_secret\n\ny\n\ny\n\ny\n\ny\nn\nn\n'
 
 # Test installation
 ./scripts/installscripts/tests/test_installation.sh
