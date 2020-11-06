2806cc95842df33f87e6a15f6bbe724a4b8a8d19
Micz Flor <micz.flor@web.de>
2020-11-06 10:14:34 +0100

    web app settings for #1122

--- htdocs/inc.header.php
+++ htdocs/inc.header.php
@@ -148,11 +148,12 @@ if(!file_exists($conf['settings_abs']."/global.conf")) {
 
 // read the global conf file
 $globalConf = parse_ini_file($conf['settings_abs']."/global.conf", $process_sections = null);
-//print "<pre>"; print_r($globalConf); print "</pre>"; //???
+print "<pre>"; print_r($globalConf); print "</pre>"; //???
 
 // assign the values from the global conf file to the vars in PHP
 $Audio_Folders_Path = $globalConf['AUDIOFOLDERSPATH'];
 $Playlists_Folders_Path = $globalConf['PLAYLISTSFOLDERPATH'];
+$Swipe_or_Place = $globalConf['SWIPEORPLACE'];
 $Second_Swipe = $globalConf['SECONDSWIPE'];
 $Second_Swipe_Pause = $globalConf['SECONDSWIPEPAUSE'];
 $Second_Swipe_Pause_Controls = $globalConf['SECONDSWIPEPAUSECONTROLS'];
--- htdocs/settings.php
+++ htdocs/settings.php
@@ -101,6 +101,22 @@ include("inc.setLanguage.php");
       </div><!-- / .row -->
     </div><!-- /.panel-body -->
 
+<div class="panel-group">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title"><a name="language"></a>
+         <i class='mdi mdi-emoticon'></i> <?php print $lang['settingsPlayoutBehaviourCard']; ?>
+      </h4>
+    </div><!-- /.panel-heading -->
+    
+    <div class="panel-body">
+      <div class="row">
+<?php
+include("inc.setPlayerBehaviourRFID.php");
+?>
+      </div><!-- / .row -->
+    </div><!-- /.panel-body -->
+
   </div><!-- /.panel -->
 </div><!-- /.panel-group -->
 
