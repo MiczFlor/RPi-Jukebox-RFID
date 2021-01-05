e8ca69fa9726031814dc5804c735584097172f2f
manajoe <mana@mm13.de>
2021-01-05 21:35:26 +0100

    Updated inc.setShutdownReduceVolume.php

--- htdocs/inc.setShutdownReduceVolume.php
+++ htdocs/inc.setShutdownReduceVolume.php
@@ -39,9 +39,9 @@ Sleep Timer Set Form
             <div class="row" style="margin-bottom:1em;">
               <div class="col-xs-6">
               <h4><?php print $lang['globalShutReduceVolume']; ?></h4>
-                <form name='shut_reduce_volume' method='post' action='<?php print $_SERVER['PHP_SELF']; ?>'>
+                <form name='shutdownwithreducingvolume' method='post' action='<?php print $_SERVER['PHP_SELF']; ?>'>
                   <div class="input-group my-group">
-                    <select id="shut_reduce_volume" name="shut_reduce_volume" class="selectpicker form-control">
+                    <select id="shutdownwithreducingvolume" name="shutdownwithreducingvolume" class="selectpicker form-control">
                         <option value='0'><?php print $lang['globalOff']; ?></option>
                     <?php
                     foreach($shut_reduce_volume_vals as $i) {
