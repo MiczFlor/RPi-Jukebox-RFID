e4a66f604e47bb4be3acd3ccc40fe538c02683dc
ChrisSoc <75833833+ChisSoc@users.noreply.github.com>
2021-01-10 10:44:55 +0100

    Re-enable start-up sound

--- scripts/startup-scripts.sh
+++ scripts/startup-scripts.sh
@@ -41,7 +41,7 @@ while [ "$STATUS" != "ACTIVE" ]; do STATUS=$(echo -e status\\nclose | nc -w 1 lo
 # play startup sound
 mpgvolume=$((32768*${AUDIOVOLSTARTUP}/100))
 echo "${mpgvolume} is the mpg123 startup volume"
-#/usr/bin/mpg123 -f -${mpgvolume} /home/pi/RPi-Jukebox-RFID/shared/startupsound.mp3
+/usr/bin/mpg123 -f -${mpgvolume} /home/pi/RPi-Jukebox-RFID/shared/startupsound.mp3
 
 #######################
 # re-scan music library
