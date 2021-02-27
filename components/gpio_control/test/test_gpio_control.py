3bdf584712d8b12901fc68cdcffd90a53112ac3d
arne123 <arne@pagelnet.de>
2021-02-27 10:16:17 +0100

    changed test_gpio to call classes

--- components/gpio_control/test/test_gpio_control.py
+++ components/gpio_control/test/test_gpio_control.py
@@ -2,7 +2,8 @@ import configparser
 import logging
 
 from mock import patch, MagicMock
-from components.gpio_control.gpio_control import get_all_devices
+from components.gpio_control.gpio_control import gpio_control
+from components.gpio_control.function_calls import phoniebox_function_calls
 
 # def test_functionCallTwoButtonsOnlyBtn2Pressed(btn1Mock, btn2Mock, functionCall1Mock, functionCall2Mock,
 #                                                functionCallBothPressedMock):
@@ -25,10 +26,14 @@ mockedFunction3.side_effect = lambda *args: print('MockFunction3 called')
 
 logging.basicConfig(level='DEBUG')
 
-
 def testMain():
     config = configparser.ConfigParser()
     config.read('./gpio_settings_test.ini')
-    devices = get_all_devices(config)
-    print(devices)
+
+    test_phoniebox_function_calls = phoniebox_function_calls()
+    gpio_controler = gpio_control(test_phoniebox_function_calls)
+
+    devices = gpio_controler.get_all_devices(config)
+    gpio_controler.print_all_devices()
+
     pass
