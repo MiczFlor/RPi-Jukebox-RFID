c1cc40ac5cb72b4903fdcc6c87ad5dd73f0b214f
T0bi79 <tobiashoppe79@web.de>
2021-04-30 17:33:58 +0200

    Tested and repaired shutdown_button class: - fixed import of SimpleButton (as in shutdown_button.py and two_button_control.py) since it threw an error under python 3. - removed input params hold_repeat and hold_time since they are not used by any instance of ShutdownButton. - configure LED GPIO port on init (LED flah function threw an error since GPIO was still uninitalized) - renamed "status" to "cancelled" for better understanding - enforce final LED switch off if action was cancelled early (might have stayed on if button was released during flashing)

--- components/gpio_control/GPIODevices/shutdown_button.py
+++ components/gpio_control/GPIODevices/shutdown_button.py
@@ -2,7 +2,10 @@ import math
 import time
 from RPi import GPIO
 import logging
-from .simple_button import SimpleButton
+try:
+    from simple_button import SimpleButton
+except ImportError:
+    from .simple_button import SimpleButton
 
 logger = logging.getLogger(__name__)
 
@@ -10,12 +13,14 @@ logger = logging.getLogger(__name__)
 class ShutdownButton(SimpleButton):
 
     def __init__(self, pin, action=lambda *args: None, name=None, bouncetime=500, edge=GPIO.FALLING,
-                 hold_time=.1, led_pin=None, time_pressed=2, hold_repeat=False, pull_up_down=GPIO.PUD_UP, iteration_time=.2):
+                 led_pin=None, time_pressed=2, pull_up_down=GPIO.PUD_UP, iteration_time=.2):
         self.led_pin = led_pin
         self.time_pressed = time_pressed
         self.iteration_time = iteration_time
+        if self.led_pin is not None:
+            GPIO.setup(self.led_pin, GPIO.OUT)
         super(ShutdownButton, self).__init__(pin=pin, action=action, name=name, bouncetime=bouncetime, edge=edge,
-                                             hold_time=hold_time, hold_repeat=hold_repeat, pull_up_down=pull_up_down,
+                                             pull_up_down=pull_up_down
                                              )
         pass
 
@@ -30,7 +35,7 @@ class ShutdownButton(SimpleButton):
             logger.debug('cannot set LED to {}: no LED pin defined'.format(status))
 
     def callbackFunctionHandler(self, *args):
-        status = False
+        cancelled = False
         n_checks = math.ceil(self.time_pressed / self.iteration_time)
         logger.debug('ShutdownButton pressed, ensuring long press for {} seconds, checking each {}s: {}'.format(
             self.time_pressed, self.iteration_time, n_checks
@@ -38,16 +43,18 @@ class ShutdownButton(SimpleButton):
         for x in range(n_checks):
             self.set_led(x & 1)
             time.sleep(.2)
-            status = not self.is_pressed
-            if status:
+            cancelled = not self.is_pressed
+            if cancelled:
                 break
-        self.set_led(status)
-        if not status:
+        if not cancelled:
             # trippel off period to indicate command accepted
             time.sleep(.6)
             self.set_led(GPIO.HIGH)
             # leave it on for the moment, it will be off when the system is down
             self.when_pressed(*args)
+        else:
+            # switch off LED if pressing was cancelled early (during flashing)
+            self.set_led(GPIO.LOW)
 
     def __repr__(self):
         return '<ShutdownButton-{}(pin {},time_pressed={},iteration_time={},led_pin={})>'.format(
