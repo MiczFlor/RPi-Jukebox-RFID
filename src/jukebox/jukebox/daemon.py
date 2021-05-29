e628308394a57296da8fe9e47de9ebf43f260c05
Chris <75833833+ChisSoc@users.noreply.github.com>
2021-05-30 00:04:10 +0200

    Future3/cfghandler (#1427)
    * Draft for daemon <-> module interface
    
    * Make Server RPC call error resiliant
    
    * Complete changeover to cfghandler

--- src/jukebox/jukebox/daemon.py
+++ src/jukebox/jukebox/daemon.py
@@ -6,43 +6,37 @@ import sys
 import signal
 import configparser
 import logging
+import importlib
+
+import zmq
 
 import jukebox.Volume
 import jukebox.System
 from player import PlayerMPD
-from jukebox.rpc.Server import RpcServer
+from jukebox.rpc.server import RpcServer
 from jukebox.NvManager import nv_manager
 from components.rfid_reader.PhonieboxRfidReader import RFID_Reader
 # from gpio_control import gpio_control
 
+import jukebox.cfghandler
+
 logger = logging.getLogger('jb.daemon')
+cfg = jukebox.cfghandler.get_handler('jukebox')
 
 
 class JukeBox:
     def __init__(self, configuration_file):
         self.nvm = nv_manager()
-        self.zmq_context = None
+        self.configuration_file = configuration_file
 
-        self.config = configparser.ConfigParser(inline_comment_prefixes=";")
-        self.config.read(configuration_file)
+        jukebox.cfghandler.load_yaml(cfg, '../../settings/jukebox.yaml')
 
-        logger.info("Starting the " + self.config.get('SYSTEM', 'BOX_NAME') + " Daemon")
-
-        if logger.isEnabledFor(logging.DEBUG):
-            self.dump_config_options(self.config, configuration_file)
+        logger.info("Starting the " + cfg.getn('system', 'box_name', default='Jukebox2') + " Daemon")
+        logger.info("Starting the " + cfg['system'].get('box_name', default='Jukebox2') + " Daemon")
 
         # setup the signal listeners
         signal.signal(signal.SIGINT, self.signal_handler)
         signal.signal(signal.SIGTERM, self.signal_handler)
-
-    def dump_config_options(self, config, filename):
-        print("\nDumping configig option from File:" + filename)
-        for section in config.sections():
-            print("[" + section + "]")
-            options = config.options(section)
-            for option in options:
-                print(option + " = " + config.get(section, option))
-            print("\n")
 
     def signal_handler(self, esignal, frame):
         # catches signal and triggers the graceful exit
@@ -53,9 +47,11 @@ class JukeBox:
         # TODO: play stop
         # TODO: Iterate over objects and tell them to exit
         # TODO: stop all threads
+        logger.info("Exiting")
 
         # save all nonvolatile data
         self.nvm.save_all()
+        jukebox.cfghandler.write_yaml(cfg, self.configuration_file, only_if_changed=True)
 
         # TODO: implement shutdown ()
 
@@ -63,34 +59,42 @@ class JukeBox:
         sys.exit(0)
 
     def run(self):
-        # Play Startup Sound
-        volume_control = jukebox.Volume.volume_control_alsa(listcards=False)
-
-        startsound_thread = threading.Thread(target=volume_control.play_wave_file,
-                                             args=[self.config.get('SYSTEM', 'STARTUP_SOUND')])
-        startsound_thread.start()
+        objects = {}
+        if 'volume' in cfg['modules']:
+            try:
+                m_volume = importlib.import_module(cfg['modules']['volume'], 'pkg.subpkg')
+            except Exception as e:
+                logger.error(f"Failed to load volume module: {cfg['modules']['volume']}. Trying without...")
+                logger.error(f"Reason: {e}")
+            else:
+                objects['volume'] = m_volume.init()
+                if 'startup_sound' in cfg['system']:
+                    startsound_thread = threading.Thread(target=m_volume.play_wave,
+                                                         args=[cfg['system']['startup_sound']],
+                                                         name='StartSound')
+                    startsound_thread.daemon = True
+                    startsound_thread.start()
+                else:
+                    logger.debug("No startup sound in config file")
 
         # load music player status
-        music_player_status = self.nvm.load(self.config.get('PLAYER', 'MUSIC_PLAYER_STATUS'))
+        music_player_status = self.nvm.load(cfg.getn('player', 'status_file'))
 
         # load card id database
-        cardid_database = self.nvm.load(self.config.get('RFID', 'CARDID_DATABASE'))
+        cardid_database = self.nvm.load(cfg.getn('rfid', 'cardid_database'))
 
         # MPD Configs
-        mpd_host = self.config.get('SYSTEM', 'MPD_HOST')
+        mpd_host = cfg.getn('mpd', 'host')
 
         # initialize Jukebox objcts
-        objects = {'volume': volume_control,
-                   'player': PlayerMPD.player_control(mpd_host, music_player_status, volume_control),
-                   'system': jukebox.System.system_control}
+        objects['player'] = PlayerMPD.player_control(mpd_host, music_player_status, None)
+        objects['system'] = jukebox.System.system_control
 
         logger.info("Init Jukebox RPC Server")
         rpcserver = RpcServer(objects)
-        if rpcserver is not None:
-            self.zmq_context = rpcserver.connect()
 
         # rfid_reader = RFID_Reader("RDM6300",{'numberformat':'card_id_float'})
-        rfid_reader = RFID_Reader("Fake", zmq_context=self.zmq_context)
+        rfid_reader = RFID_Reader("Fake", zmq_address='inproc://JukeBoxRpcServer', zmq_context=zmq.Context.instance())
         if rfid_reader is not None:
             rfid_reader.set_cardid_db(cardid_database)
             rfid_reader.reader.set_card_ids(list(cardid_database))     # just for Fake Reader to be aware of card ids
@@ -102,8 +106,9 @@ class JukeBox:
         # TODO: GPIO not yet integrated
         gpio_config = None
         if gpio_config is not None:
-            gpio_config = configparser.ConfigParser(inline_comment_prefixes=";")
-            gpio_config.read(self.config.get('GPIO', 'GPIO_CONFIG'))
+            pass
+            # gpio_config = configparser.ConfigParser(inline_comment_prefixes=";")
+            # gpio_config.read(self.config.get('GPIO', 'GPIO_CONFIG'))
 
             # phoniebox_function_calls = function_calls.phoniebox_function_calls()
             # gpio_controler = gpio_control(phoniebox_function_calls)
---
+++ src/jukebox/jukebox/rpc/server.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import nanotime
+import zmq
+import json
+import logging
+import jukebox.cfghandler
+
+logger = logging.getLogger('jb.rpc_server')
+cfg = jukebox.cfghandler.get_handler('jukebox')
+
+
+class RpcServer:
+    def __init__(self, objects, context=None):
+        # Get the global context (will be created if non-existing)
+        self.context = context or zmq.Context.instance()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind('inproc://JukeBoxRpcServer')
+        port = cfg.getn('rpc', 'port_cmd', default=5555)
+        self.socket.bind(f'tcp://*:{port}')
+        self.socket.setsockopt(zmq.LINGER, 200)
+        self.objects = objects
+        self._keep_running = True
+        logger.info(f"Connected to port '{port}'")
+
+    def terminate(self):
+        self._keep_running = False
+
+    def execute(self, module, method, args, kwargs):
+        """
+        Call the method in module with args and kwargs
+
+        Arguments and keyword arguments will be unpacked when calling the function
+        :param module: The plugin module
+        :param method: The registers function within the plugin module
+        :param args: Set with arguments passed to the function. Pass empty set if unneeded
+        :param kwargs: Dict with keyword arguments. Pass an empty dict if unneeded
+        :return: The return value of the method in the RPC protocol format or an error message
+        """
+        if module in self.objects:
+            method_attr = getattr(self.objects[module], method, None)
+            if callable(method_attr):
+                try:
+                    response = {'result': method_attr(*args, **kwargs)}
+                except Exception as e:
+                    response = {'error': {'code': -1, 'message': f"Error while executing method: '{method_attr}' in '{module}'. Reason: {e}"}}
+            else:
+                response = {'error': {'code': -1, 'message': f"Method not callable: '{method}' in '{module}'"}}
+        else:
+            response = {'error': {'code': -1, 'message': f"Unknown plugin object: '{module}'"}}
+        return response
+
+    def run(self):
+        self._keep_running = True
+        # TODO: check if connected, otherwise connect or exit?
+
+        while self._keep_running:
+            #  Wait for next request from client
+            message = self.socket.recv()
+            nt = nanotime.now().nanoseconds()
+
+            client_request = json.loads(message)
+
+            logger.debug(f"Request: {client_request}")
+
+            # in difference to jsonrpc https://www.jsonrpc.org/specification
+            # {"jsonrpc": "2.0", "method": "subtract", "params": {"subtrahend": 23, "minuend": 42}, "id": 3}
+            # a additional required parameter "object" is used:
+            # {'object':'','method':'','params':{},id:''}
+            module = client_request.get('plugin')
+            if module is not None:
+                method = client_request.get('method')
+                if method is not None:
+                    args = client_request.get('args', set())
+                    kwargs = client_request.get('kwargs', {})
+                    response = self.execute(module, method, args, kwargs)
+                else:
+                    response = {'error': {'code': -1, 'message': "Missing mandatory parameter 'method'."}}
+            else:
+                response = {'error': {'code': -1, 'message': "Missing mandatory parameter 'object'."}}
+
+            if 'tsp' in client_request:
+                response['total_processing_time'] = (nt - int(client_request['tsp'])) / 1000000
+                logger.debug("Execute: Processing time: {:2.3f} ms".format(response['total_processing_time']))
+
+            if 'id' in client_request:
+                response['id'] = client_request.get('id')
+
+            if 'error' in response:
+                logger.error(f"Execute: {response['error']['message']}")
+
+            logger.debug(f"Execute: {response}")
+
+            #  Send reply back to client
+            self.socket.send_string(json.dumps(response))
