48eb08195c19fa0c4636fb265463a91aebb2fe36
Patrick Beer <patrick@pabera.com>
2021-07-06 20:39:48 +0200

    Install script: organize output better

--- install-jukebox.sh
+++ install-jukebox.sh
@@ -2,14 +2,21 @@
 # Handle language configuration
 export LC_ALL=C
 
+# Constants
+INSTALL_ID=$(date +%s)
 HOME_DIR="/home/pi"
 INSTALLATION_DIR="${HOME_DIR}/RPi-Jukebox-RFID"
 GIT_URL="https://github.com/MiczFlor/RPi-Jukebox-RFID.git"
 GIT_BRANCH="future3/webapp"
 
 # Log installation for debugging reasons
-exec >> $HOME_DIR/INSTALL.log 2>&1
+INSTALLATION_LOGFILE="$HOME_DIR/INSTALL-$INSTALL_ID.log"
+exec 3>&1 1>>${INSTALLATION_LOGFILE} 2>&1
+echo "Log start: $INSTALL_ID"
 
+### Method definitions
+
+# Welcome Screen
 welcome() {
     clear
 echo "#####################################################
@@ -20,22 +27,22 @@ echo "#####################################################
 #                                                   #
 #####################################################
 
-You are turning your Raspberry Pi into a Phoniebox. Good choice!"
+You are turning your Raspberry Pi into a Phoniebox. Good choice!" 1>&3
 
-  read -rp "Do you want to install? [Y/n] " response
+  read -rp "Do you want to install? [Y/n] " response 1>&3
   case "$response" in
     [nN][oO]|[nN])
       exit
       ;;
     *)
-      echo "Installation continues..."
+      echo "Starting installation ..." 1>&3
       ;;
   esac
 }
 
-# Update pi config
+# Update RPi configuration
 set_raspi_config() {
-  echo "Set default raspi-config"
+  echo "Set default raspi-config" | tee /dev/fd/3
   # Source: https://raspberrypi.stackexchange.com/a/66939
   # Autologin
   sudo raspi-config nonint do_boot_behaviour B2
@@ -45,17 +52,19 @@ set_raspi_config() {
   sudo iwconfig wlan0 power off
   # Switch off Bluetooth to save energy
   sudo systemctl stop bluetooth
+  # Skip interactive Samba WINS config dialog
+  echo "samba-common samba-common/dhcp boolean false" | sudo debconf-set-selections
 }
 
 # Update System
 update_os() {
-  echo "Updating Raspberry Pi. This will take a while ..."
-  sudo apt-get -qq -y update; sudo apt-get -qq -y full-upgrade > /dev/null; sudo apt-get -qq -y autoremove
+  echo "Updating Raspberry Pi OS" | tee /dev/fd/3
+  sudo apt-get -qq -y update; sudo apt-get -qq -y full-upgrade > /dev/null; sudo apt-get -qq -y autoremove > /dev/null
 }
 
 # Install Dependencies
 install_jukebox_dependencies() {
-  echo "Install Jukebox dependencies"
+  echo "Install Jukebox dependencies" | tee /dev/fd/3
   sudo apt-get -qq -y update; sudo apt-get -qq -y install \
     at git wget \
     mpd mpc \
@@ -68,21 +77,20 @@ install_jukebox_dependencies() {
     --no-install-recommends \
     --allow-downgrades \
     --allow-remove-essential \
-    --allow-change-held-packages
+    --allow-change-held-packages > /dev/null
   sudo rm -rf /var/lib/apt/lists/*
 
   # Install Python
-  echo "  Install Python"
   sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
 
   # Install Node
   if which node > /dev/null; then
-    echo "  Update NodeJS"
+    echo "  Found existing NodeJS. Hence, updating NodeJS" | tee /dev/fd/3
     sudo npm cache clean -f
     sudo n --quiet latest
     sudo npm update --silent -g
   else
-    echo "  Install NodeJS"
+    echo "  Install NodeJS" | tee /dev/fd/3
     curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
     sudo apt-get -qq -y install nodejs
     sudo npm install --silent -g n npm pm2 serve
@@ -92,11 +100,11 @@ install_jukebox_dependencies() {
   # Because the latest stable release of ZMQ does not support WebSockets
   # we need to compile the latest version in Github
   # As soon WebSockets support is stable in ZMQ, this can be removed
-  echo "  Install pyzmq"
+  echo "  Install pyzmq" | tee /dev/fd/3
   ZMQ_VERSION="4.3.4"
   ZMQ_PREFIX="/usr/local"
 
-  if pip3 list | grep -F pyzmq >> /dev/null; then
+  if ! pip3 list | grep -F pyzmq >> /dev/null; then
     cd ${HOME_DIR} && mkdir libzmq && cd libzmq
     # TODO: Official release fails to compile on RPi (RPi freezes) - check TEMP solution
     # wget https://github.com/zeromq/libzmq/releases/download/v${ZMQ_VERSION}/zeromq-${ZMQ_VERSION}.tar.gz -O libzmq.tar.gz
@@ -113,28 +121,28 @@ install_jukebox_dependencies() {
     pip3 install setuptools # Required for the following command to work, not sure why
     pip3 install -q --pre pyzmq --install-option=--enable-drafts --install-option=--zmq=bundled
   else
-    echo "  Skipping. pyzmq already installed"
+    echo "  Skipping. pyzmq already installed" | tee /dev/fd/3
   fi
 }
 
 # Install Jukebox
 install_jukebox() {
-  echo "Install Jukebox"
+  echo "Install Jukebox" | tee /dev/fd/3
   cd ${HOME_DIR}
 
   if [ -d "$INSTALLATION_DIR" ]; then
     cd ${INSTALLATION_DIR}
     if [[ `git status --porcelain` ]]; then
-      echo "  Found local changes in git repository. Moving them to backup branch 'local-backup' and git stash"
+      echo "  Found local changes in git repository. Moving them to backup branch 'local-backup-$INSTALL_ID' and git stash" | tee /dev/fd/3
       # Changes
       git fetch --all
-      git checkout -b local-backup
+      git checkout -b local-backup-$INSTALL_ID
       git stash
       git checkout $GIT_BRANCH
       git reset --hard origin/$GIT_BRANCH
     else
       # No changes
-      echo "  Updating version"
+      echo "  Updating version" | tee /dev/fd/3
       git pull
     fi
   else
@@ -142,11 +150,11 @@ install_jukebox() {
   fi
 
   # Install Python dependencies
-  echo "  Install Python dependencies"
+  echo "  Install Python dependencies" | tee /dev/fd/3
   pip3 install -q --no-cache-dir -r ${INSTALLATION_DIR}/requirements.txt
 
   # Install Node dependencies
-  echo "  Install web application"
+  echo "  Install web application" | tee /dev/fd/3
   cd ${INSTALLATION_DIR}/src/webapp
   npm install --silent && npm install --silent react-scripts@3.4.1 -g
   rm -rf build
@@ -159,12 +167,13 @@ configure_samba() {
   local SMB_USER="pi"
   local SMB_PASSWD="raspberry"
 
-  echo "Configure Samba"
+  echo "Configure Samba" | tee /dev/fd/3
   # Samba has not been configured
   if grep -q "## Jukebox Samba Config" "$SMB_CONF"; then
-    echo "  Skipping. Already set up!"
+    echo "  Skipping. Already set up!" | tee /dev/fd/3
   else
-    echo "samba-common samba-common/dhcp boolean false" | sudo debconf-set-selections # Skip interactive Samba WINS config dialog
+    # Create Samba user
+    (echo "${SMB_PASSWD}"; echo "${SMB_PASSWD}") | sudo smbpasswd -s -a $SMB_USER
 
     sudo chown root:root $SMB_CONF
     sudo chmod 777 $SMB_CONF
@@ -195,19 +204,16 @@ configure_samba() {
 EOF
 
     sudo chmod 644 $SMB_CONF
-
-    # Create Samba user
-    (echo "${SMB_PASSWD}"; echo "${SMB_PASSWD}") | sudo smbpasswd -s -a $SMB_USER
   fi
 }
 
 main() {
-  welcome()
-  set_raspi_config()
-  update_os()
-  install_jukebox_dependencies()
-  configure_samba()
-  install_jukebox()
+  welcome
+  set_raspi_config
+  update_os
+  install_jukebox_dependencies
+  configure_samba
+  install_jukebox
 }
 
 start=$(date +%s)
@@ -219,4 +225,4 @@ runtime=$((end-start))
 ((h=${runtime}/3600))
 ((m=(${runtime}%3600)/60))
 ((s=${runtime}%60))
-echo "Done (in ${h}h ${m}m ${s}s)."
+echo "Done (in ${h}h ${m}m ${s}s)." | tee /dev/fd/3
