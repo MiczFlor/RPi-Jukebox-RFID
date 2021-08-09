31ed347b98f68dc0f8bdd5c695d59555defdb26c
t0b3 <t0b3@users.noreply.github.com>
2021-08-09 13:48:44 +0200

    install alsa-utils (needed for buster lite)
    #1255 misses the alsa-utils package that is needed make it work on buster-lite
    see #1467

--- installation/install-jukebox.sh
+++ installation/install-jukebox.sh
@@ -110,7 +110,7 @@ install_jukebox_dependencies() {
     samba samba-common-bin \
     python3 python3-dev python3-pip python3-setuptools python3-mutagen python3-gpiozero \
     ffmpeg \
-    alsa-tools \
+    alsa-utils \
     --no-install-recommends \
     --allow-downgrades \
     --allow-remove-essential \
