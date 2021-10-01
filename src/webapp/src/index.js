35218789f466bdac4c3658c21449ec973a244046
pabera <1260686+pabera@users.noreply.github.com>
2021-10-01 11:05:22 +0200

    Update npm dependencies

--- src/webapp/src/index.js
+++ src/webapp/src/index.js
@@ -4,7 +4,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -13,8 +12,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.querySelector('#root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
