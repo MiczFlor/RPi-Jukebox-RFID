0d1c5c4568a6750889dfc33f49e6865875f44a5e
ChisSoc <75833833+ChisSoc@users.noreply.github.com>
2021-10-13 21:00:05 +0200

    Quality update to sphinx runner script

--- run_sphinx.sh
+++ run_sphinx.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env bash
 
-# Runner script for sphinx documentation build
-# Usage:
-# (a) run_sphinx.sh for incremental build
-# (b) run_sphinx.sh clean for clean re-build
+usage() {
+  echo "Runner script for sphinx documentation build"
+  echo "Usage:"
+  echo "   ./run_sphinx.sh    : incremental build"
+  echo "   ./run_sphinx.sh -c : clean re-build"
+  exit 1
+}
 
 # Change working directory to location of script
 SOURCE=${BASH_SOURCE[0]}
 SCRIPT_DIR="$(dirname "$SOURCE")"
 cd "$SCRIPT_DIR/docs/sphinx" || (echo "Could not change to docs directory" && exit 1)
 
-# Run sphinx
-make "$@" html
+BUILD_DIR=_build
+
+CLEAN_BUILD=false
+while getopts ":ch" opt;
+do
+  case ${opt} in
+    c ) CLEAN_BUILD=true
+      ;;
+    h ) usage
+      ;;
+    \? ) usage
+      ;;
+  esac
+done
+
+if [[ $CLEAN_BUILD = true ]]; then
+  echo "Cleaning $BUILD_DIR"
+  rm -rf $BUILD_DIR
+  echo "Building docs"
+  sphinx-build -W --keep-going -T -a -E -b html . $BUILD_DIR
+else
+  echo "Building docs"
+  sphinx-build -W --keep-going -T -b html . $BUILD_DIR
+fi
+
