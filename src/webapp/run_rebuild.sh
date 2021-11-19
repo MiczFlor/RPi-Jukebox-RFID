83cc52e8f25c5a992456c33cd31ad7768189d79d
pabera <1260686+pabera@users.noreply.github.com>
2021-11-19 19:54:22 +0100

    [Install Webapp] Undo Memory

--- src/webapp/run_rebuild.sh
+++ src/webapp/run_rebuild.sh
@@ -40,34 +40,31 @@ FREEMEM=$(free -mt | sed -rn 's/Total:\s+[0-9]+\s+[0-9]+\s+([0-9])/\1/p')
 echo "Max memory: ${MEMORY}"
 echo "Free memory: ${FREEMEM}"
 
-if [ "$MEMORY" -lt 600000 ]; then
-  if [[ -z $NODEMEM ]]; then
-    # Keep a buffer of minimum 20 MB
-    if [[ $FREEMEM -gt 1044 ]]; then
-      NODEMEM=1024
-    elif [[ $FREEMEM -gt 532 ]]; then
-      NODEMEM=512
-    elif [[ $FREEMEM -gt 276 ]]; then
-      NODEMEM=256
-    else
-      echo "ERROR: Not enough memory available on system. Please increase swap size to give at least 276 MByte free memory."
-      echo "Current free memory = $FREEMEM"
-      echo "Hint: if only a little memory is missing, stopping spocon, mpd, and jukebox-daemon might give you enough space"
-      exit 1
-    fi
-  fi
-
-  if [[ $NODEMEM -gt $FREEMEM ]]; then
-    echo "ERROR: Requested node memory setting is larger than available free memory: $NODEMEM MB > $FREEMEM MB"
+if [[ -z $NODEMEM ]]; then
+  # Keep a buffer of minimum 20 MB
+  if [[ $FREEMEM -gt 1044 ]]; then
+    NODEMEM=1024
+  elif [[ $FREEMEM -gt 532 ]]; then
+    NODEMEM=512
+  elif [[ $FREEMEM -gt 276 ]]; then
+    NODEMEM=256
+  else
+    echo "ERROR: Not enough memory available on system. Please increase swap size to give at least 276 MByte free memory."
+    echo "Current free memory = $FREEMEM"
+    echo "Hint: if only a little memory is missing, stopping spocon, mpd, and jukebox-daemon might give you enough space"
     exit 1
   fi
+fi
+
+if [[ $NODEMEM -gt $FREEMEM ]]; then
+  echo "ERROR: Requested node memory setting is larger than available free memory: $NODEMEM MB > $FREEMEM MB"
+  exit 1
+fi
 
   export NODE_OPTIONS=--max-old-space-size=${NODEMEM}
 
   echo "Setting Node Options:"
   env | grep NODE
-fi
-
 # In rare cases you will need to update the npm dependencies
 # This is the case when the file package.json changed
 if [[ $UPDATE_DEPENDENCIES == true ]]; then
