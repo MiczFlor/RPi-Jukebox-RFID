2500ce449b942263951cbc623a47a104de906394
pabera <1260686+pabera@users.noreply.github.com>
2021-11-22 19:57:53 +0100

    Update order of installation, move webapp to the top

--- installation/routines/install.sh
+++ installation/routines/install.sh
@@ -1,15 +1,15 @@
 install() {
   customize_options
   clear 1>&3
+  # init_git_repo
   set_raspi_config
   if [ "$DISABLE_SSH_QOS" = true ] ; then set_ssh_qos; fi;
   if [ "$UPDATE_RASPI_OS" = true ] ; then update_raspi_os; fi;
-  setup_jukebox_core
-  # init_git_repo
-  if [ "$SETUP_MPD" = true ] ; then setup_mpd; fi;
-  if [ "$ENABLE_SAMBA" = true ] ; then setup_samba; fi;
   if [ "$ENABLE_WEBAPP" = true ] ; then setup_jukebox_webapp; fi;
   if [ "$ENABLE_KIOSK_MODE" = true ] ; then setup_kiosk_mode; fi;
+  setup_jukebox_core
+  if [ "$SETUP_MPD" = true ] ; then setup_mpd; fi;
+  if [ "$ENABLE_SAMBA" = true ] ; then setup_samba; fi;
   setup_rfid_reader
   optimize_boot_time
   if [ "$ENABLE_AUTOHOTSPOT" = true ] ; then setup_autohotspot; fi;
