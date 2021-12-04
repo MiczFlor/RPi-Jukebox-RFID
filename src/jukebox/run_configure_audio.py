7f93823e72b2be434261c4a7cd9d0039f4808c73
ChisSoc <75833833+ChisSoc@users.noreply.github.com>
2021-12-04 06:28:13 +0100

    Check if jukebox daemon is running in configuration scripts

--- src/jukebox/run_configure_audio.py
+++ src/jukebox/run_configure_audio.py
@@ -11,20 +11,12 @@ import argparse
 import pulsectl
 
 import misc.inputminus as pyil
+from misc.inputminus import msg_highlight
 from misc.simplecolors import Colors
 import jukebox.cfghandler
-
-deliminator_length = 79
-
-
-def msg_highlight(msg, color=Colors.lightblue):
-    print(f"\n{color}" + "*" * deliminator_length)
-    print(msg)
-    print("*" * deliminator_length + f"{Colors.reset}")
-
-
-def check_jukebox_running():
-    pass
+import jukebox.plugs
+jukebox.plugs.ALLOW_DIRECT_IMPORTS = True
+import components.hostif.linux as host  # noqa: E402
 
 
 def query_sinks():
@@ -40,18 +32,20 @@ def query_sinks():
                                  prompt_hint=True, blank=0)
     primary = sinks[primary_idx].name
     print(f"Primary audio output = {primary}\n")
-    secondary_idx = pyil.input_int("Secondary audio output (typically bluetooth)? Set to -1 for empty.",
-                                   min=-1, max=len(sinks) - 1,
-                                   prompt_color=Colors.lightgreen,
-                                   prompt_hint=True, blank=-1)
+
     secondary = None
     toggle_on_connect = False
-    if secondary_idx >= 0:
-        secondary = sinks[secondary_idx].name
-        print(f"Secondary audio output = {secondary}\n")
-        toggle_on_connect = pyil.input_yesno("Automatically toggle output on connection of secndary device?",
-                                             prompt_color=Colors.lightgreen,
-                                             prompt_hint=True, blank=True)
+    if len(sinks) > 1:
+        secondary_idx = pyil.input_int("Secondary audio output (typically bluetooth)? Set to -1 for empty.",
+                                       min=-1, max=len(sinks) - 1,
+                                       prompt_color=Colors.lightgreen,
+                                       prompt_hint=True, blank=-1)
+        if secondary_idx >= 0:
+            secondary = sinks[secondary_idx].name
+            print(f"Secondary audio output = {secondary}\n")
+            toggle_on_connect = pyil.input_yesno("Automatically toggle output on connection of secondary device?",
+                                                 prompt_color=Colors.lightgreen,
+                                                 prompt_hint=True, blank=True)
     return primary, secondary, toggle_on_connect
 
 
@@ -78,17 +72,17 @@ def configure_jukebox(filename, primary, secondary, toggle_on_connect):
     cfg_jukebox.save()
 
 
-def welcome():
+def welcome(config_file):
     msg_highlight('The Jukebox audio output configuration tool')
     print("""
 Please note:
  - Primary output must be available on system boot - i.e. not a bluetooth device
  - Secondary output is typically a bluetooth device
- - Connect your bluetooth device before running this script (or run it again later)
- - Will overwrite your audio output configuration in 'filename'
- - Exit all running Jukeboxes (including services) before continuing
-     $ sudo systemctl stop jukebox-daemon
- - Checkout the documentation at TBD
+ - Connect your bluetooth device before running this script (or run it again later)""")
+    print(f" - Will replace your audio output configuration in\n   '{config_file}'")
+    print(""" - Exit all running Jukeboxes (including services) before continuing
+     $ systemctl --user stop jukebox-daemon
+ - Checkout the documentation page 'Audio Configuration'
  - If you are not sure which device is which, you can try them with
      $ paplay -d sink_name /usr/share/sounds/alsa/Front_Center.wav
  - To get a list of all sinks, check out below list or use
@@ -112,7 +106,15 @@ def main():
                            metavar="FILE")
     args = argparser.parse_args()
 
-    welcome()
+    welcome(args.conf.name)
+
+    if host.is_any_jukebox_service_active():
+        msg_highlight('Jukebox service is running!')
+        print("\nPlease stop jukebox-daemon service and restart tool")
+        print("$ systemctl --user stop jukebox-daemon\n\n")
+        print("Don't forget to start the service again :-)")
+        return
+
     primary, secondary, toggle_on_connect = query_sinks()
     configure_jukebox(args.conf.name, primary, secondary, toggle_on_connect)
     goodbye()
