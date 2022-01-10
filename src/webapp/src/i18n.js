fe7add40cee3028894cc9d20ebba883b2298df06
pabera <1260686+pabera@users.noreply.github.com>
2022-01-10 09:01:19 +0100

    Move translation into their own files and load them async

--- src/webapp/src/i18n.js
+++ src/webapp/src/i18n.js
@@ -1,8 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 
+// Based on https://dev.to/adrai/how-to-properly-internationalize-a-react-application-using-i18next-3hdb
 i18n
+  // i18next-http-backend
+  // loads translations from your server
+  // https://github.com/i18next/i18next-http-backend
+  .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -16,13 +22,6 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    resources: {
-      en: {
-        translation: {
-          // here we will place our translations...
-        }
-      }
-    }
   });
 
 export default i18n;
