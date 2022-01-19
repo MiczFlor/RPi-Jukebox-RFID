e12a53bbca5e6c02b3a0f3d936ed47dc6b9eae77
pabera <1260686+pabera@users.noreply.github.com>
2022-01-19 10:36:29 +0100

    Update factory naming

--- src/jukebox/components/players/__init__.py
+++ src/jukebox/components/players/__init__.py
@@ -5,11 +5,11 @@ import jukebox.plugs as plugin
 from .mpd import MpdPlayerBuilder
 from .spotify import SpotifyPlayerBuilder
 
-logger = logging.getLogger('jb.player')
+logger = logging.getLogger('jb.players')
 cfg = jukebox.cfghandler.get_handler('jukebox')
 
 
-class PlayerFactory:
+class PlayersFactory:
     def __init__(self):
         self._builders = {}
 
@@ -22,13 +22,13 @@ class PlayerFactory:
             raise ValueError(key)
         return builder(**kwargs)
 
-factory: PlayerFactory
+factory: PlayersFactory
 
 
 @plugin.initialize
 def initialize():
     global players
-    players = PlayerFactory()
+    players = PlayersFactory()
     players.register_builder('Spotify', SpotifyPlayerBuilder)
     players.register_builder('MPD', MpdPlayerBuilder)
 
