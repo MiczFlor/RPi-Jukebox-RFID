b62885011ec7bd9db658a3c4ea83b63733344975
ChisSoc <75833833+ChisSoc@users.noreply.github.com>
2022-09-04 22:39:05 +0200

    Remove unused code

--- src/jukebox/components/music_cover_art/__init__.py
+++ src/jukebox/components/music_cover_art/__init__.py
@@ -6,7 +6,6 @@ Read all cover art from music save it to a cache for the UI to load
 import logging
 import jukebox.cfghandler
 import jukebox.plugs as plugin
-import components.playern.backends.mpd.musiclib
 
 logger = logging.getLogger('jb.music_cover_art')
 cfg = jukebox.cfghandler.get_handler('jukebox')
@@ -14,10 +13,7 @@ cfg = jukebox.cfghandler.get_handler('jukebox')
 
 class MusicCoverArt:
     def __init__(self):
-        self.music_library_path = components.playern.backends.mpd.musiclib.get_music_library_path()
-
-        if self.music_library_path is None:
-            logger.error("Missing config, can't initialize plugin")
+        pass
 
     @plugin.tag
     def get_by_filename_as_base64(self, audio_src: str):
