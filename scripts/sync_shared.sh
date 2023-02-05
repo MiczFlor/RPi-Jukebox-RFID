06604d3cb06cbc463d8e6a8912880a7570bc3087
AlvinSchiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-02-05 23:47:33 +0100

    Settings in conf ausgelagert. Weitere Anpassungen im Abluaf
    Enabled Flag hinzugefuegt, mit fruehem ausstieg.
    rsync command ignoriert nun hinzufuegen der "folder.conf" als Aenderung.
    - hierdurch sync nicht nur bei erstem Scan sondern immer moeglich
    Ordner auf Remote werden initialisiert wenn nicht vorhanden.

--- scripts/sync_shared.sh
+++ scripts/sync_shared.sh
@@ -23,15 +23,6 @@ NOW=`date +%Y-%m-%d.%H:%M:%S`
 PATHDATA="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SHORTCUTSPATH="$PATHDATA/../shared/shortcuts"
 
-# Edit 
-REMOTESERVER="test.schiller-dev.de"
-REMOTEPORT=22322
-#REMOTESYNCPATH="/home/pi/test/Phoniebox/"
-REMOTESYNCPATH="/mnt/station-phoniebox/"
-
-# Common
-SYNCSHORTCUTSPATH="${REMOTESYNCPATH}shortcuts/"
-SYNCSAUDIOFILESPATH="${REMOTESYNCPATH}audiofolders/"
 
 #############################################################
 # $DEBUG TRUE|FALSE
@@ -40,43 +31,69 @@ SYNCSAUDIOFILESPATH="${REMOTESYNCPATH}audiofolders/"
 
 if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "########### SCRIPT sync_shared.sh ($NOW) ##" >> ${PATHDATA}/../logs/debug.log; fi
 
-###########################################################
+#############################################################
 # Read global configuration file (and create if not exists)
 # create the global configuration file from single files - if it does not exist
 if [ ! -f ${PATHDATA}/../settings/global.conf ]; then
     . ${PATHDATA}/inc.writeGlobalConfig.sh
 fi
 . ${PATHDATA}/../settings/global.conf
-###########################################################
 
 #############################################################
-# Get args from command line (see Usage above)
-# Read the args passed on by the command line
-# see following file for details:
-. ${PATHDATA}/inc.readArgsFromCommandLine.sh
+# Read configuration file
+# create the configuration file from sample - if it does not exist
+if [ ! -f ${PATHDATA}/../settings/sync_shared.conf ]; then
+	echo "sync_shared.conf not present"
+	if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Conf not present. Write from sample" >> ${PATHDATA}/../logs/debug.log; fi
+    cp ${PATHDATA}/../settings/sync_shared.conf.sample ${PATHDATA}/../settings/sync_shared.conf
+    # change the read/write so that later this might also be editable through the web app
+    sudo chown -R pi:www-data ${PATHDATA}/../settings/sync_shared.conf
+    sudo chmod -R 775 ${PATHDATA}/../settings/sync_shared.conf
+fi
 
-if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR COMMAND: ${COMMAND}" >> ${PATHDATA}/../logs/debug.log; fi
-if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR CARDID: ${CARDID}" >> ${PATHDATA}/../logs/debug.log; fi
-if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR FOLDER: ${FOLDER}" >> ${PATHDATA}/../logs/debug.log; fi
-if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR VALUE: ${VALUE}" >> ${PATHDATA}/../logs/debug.log; fi
+. ${PATHDATA}/../settings/sync_shared.conf
 
 
-case $COMMAND in
-    shortcuts)
-		if [ ! -f "${SHORTCUTSPATH}/${CARDID}" ]; then
-			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Shortcut for $CARDID does not exist in $SHORTCUTSPATH -> syncing" >> $PATHDATA/../logs/debug.log; fi
+if [ "${SYNCSHAREDENABLED}" != "TRUE" ]; then
+	if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: disabled" >> ${PATHDATA}/../logs/debug.log; fi
 			
-			if nc -z $REMOTESERVER -w 1 $REMOTEPORT ; then
+else
+	if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: enabled" >> ${PATHDATA}/../logs/debug.log; fi
+	
+	# Set local vars
+	SYNCSHORTCUTSPATH="${SYNCSHAREDREMOTEPATH}shortcuts/"
+	SYNCAUDIOFOLDERSPATH="${SYNCSHAREDREMOTEPATH}audiofolders/"
+
+	#############################################################
+	# Get args from command line (see Usage above)
+	# Read the args passed on by the command line
+	# see following file for details:
+	. ${PATHDATA}/inc.readArgsFromCommandLine.sh
+
+	if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR COMMAND: ${COMMAND}" >> ${PATHDATA}/../logs/debug.log; fi
+	if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR CARDID: ${CARDID}" >> ${PATHDATA}/../logs/debug.log; fi
+	if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR FOLDER: ${FOLDER}" >> ${PATHDATA}/../logs/debug.log; fi
+	if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR VALUE: ${VALUE}" >> ${PATHDATA}/../logs/debug.log; fi
+
+
+	case $COMMAND in
+		shortcuts)
+			if nc -z $SYNCSHAREDREMOTESERVER -w 1 $SYNCSHAREDREMOTEPORT ; then
+			
+				if [ ! -d "${SYNCSHORTCUTSPATH}" ]; then
+					mkdir "${SYNCSHORTCUTSPATH}"
+					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCSHORTCUTSPATH} does not exist. created" >> ${PATHDATA}/../logs/debug.log; fi
+				fi
 			
 				if [ -f "${SYNCSHORTCUTSPATH}${CARDID}" ]; then
-					RSYNCSHORTCUTSCMD=$(rsync -azi --no-o --no-g "${SYNCSHORTCUTSPATH}${CARDID}" "${SHORTCUTSPATH}")
+					RSYNCSHORTCUTSCMD=$(rsync -azi --no-o --no-g --no-times "${SYNCSHORTCUTSPATH}${CARDID}" "${SHORTCUTSPATH}")
 					
 					if [ $? -eq 0 -a -n "${RSYNCSHORTCUTSCMD}" ]; then
 						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: executed rsync ${RSYNCSHORTCUTSCMD}" >> ${PATHDATA}/../logs/debug.log; fi
 						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: files copied. setting rights" >> ${PATHDATA}/../logs/debug.log; fi
 						
 						sudo chown pi:www-data "${SHORTCUTSPATH}/${CARDID}"
-						sudo chmod -R 777 "${SHORTCUTSPATH}/${CARDID}"
+						sudo chmod -R 775 "${SHORTCUTSPATH}/${CARDID}"
 						
 					else
 						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: nothing changed" >> ${PATHDATA}/../logs/debug.log; fi
@@ -89,26 +106,24 @@ case $COMMAND in
 			else 
 				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Server is NOT reachable" >> ${PATHDATA}/../logs/debug.log; fi
 			fi
+			;;
+		audiofolders)
+			if nc -z $SYNCSHAREDREMOTESERVER -w 1 $SYNCSHAREDREMOTEPORT ; then
 			
-		else 
-			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Shortcut for $CARDID does exist in $SHORTCUTSPATH -> not syncing" >> $PATHDATA/../logs/debug.log; fi
-		fi
-        ;;
-	audiofolders)
-		if [ ! -d "${AUDIOFOLDERSPATH}/${FOLDER}" ]; then
-			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder $FOLDER does not exist in $AUDIOFOLDERSPATH -> syncing" >> $PATHDATA/../logs/debug.log; fi
+				if [ ! -d "${SYNCAUDIOFOLDERSPATH}" ]; then
+					mkdir "${SYNCAUDIOFOLDERSPATH}"
+					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCAUDIOFOLDERSPATH} does not exist. created" >> ${PATHDATA}/../logs/debug.log; fi
+				fi
 			
-			if nc -z $REMOTESERVER -w 1 $REMOTEPORT ; then
-			
-				if [ -d "${SYNCSAUDIOFILESPATH}${FOLDER}" ]; then
-					RSYNCSAUDIOFILES=$(rsync -azi --no-o --no-g "${SYNCSAUDIOFILESPATH}${FOLDER}/" "${AUDIOFOLDERSPATH}/${FOLDER}/")
+				if [ -d "${SYNCAUDIOFOLDERSPATH}${FOLDER}" ]; then
+					RSYNCSAUDIOFILES=$(rsync -azi --no-o --no-g --no-times "${SYNCAUDIOFOLDERSPATH}${FOLDER}/" "${AUDIOFOLDERSPATH}/${FOLDER}/")
 					
 					if [ $? -eq 0 -a -n "${RSYNCSAUDIOFILES}" ]; then
 						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: executed rsync ${RSYNCSAUDIOFILES}" >> ${PATHDATA}/../logs/debug.log; fi
 						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: files copied. setting rights and update database" >> ${PATHDATA}/../logs/debug.log; fi
 						
 						sudo chown pi:www-data "${AUDIOFOLDERSPATH}/${FOLDER}"
-						sudo chmod -R 777 "${AUDIOFOLDERSPATH}/${FOLDER}"
+						sudo chmod -R 775 "${AUDIOFOLDERSPATH}/${FOLDER}"
 						sudo mpc update --wait "${AUDIOFOLDERSPATH}/${FOLDER}" > /dev/null 2>&1
 						
 					else
@@ -116,19 +131,17 @@ case $COMMAND in
 					fi
 					
 				else 
-					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder $FOLDER not found in REMOTE $SYNCSAUDIOFILESPATH" >> ${PATHDATA}/../logs/debug.log; fi
+					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder $FOLDER not found in REMOTE $SYNCAUDIOFOLDERSPATH" >> ${PATHDATA}/../logs/debug.log; fi
 				fi
 				
 			else 
 				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Server is NOT reachable" >> ${PATHDATA}/../logs/debug.log; fi
 			fi
-			
-		else
-			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder $FOLDER does exist in $AUDIOFOLDERSPATH -> not syncing" >> $PATHDATA/../logs/debug.log; fi
-		fi
-        ;;
-	*)
-        echo Unknown COMMAND $COMMAND
-        if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Unknown COMMAND ${COMMAND} CARDID ${CARDID} FOLDER ${FOLDER} VALUE ${VALUE}" >> ${PATHDATA}/../logs/debug.log; fi
-        ;;
-esac
+			;;
+		*)
+			echo Unknown COMMAND $COMMAND
+			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Unknown COMMAND ${COMMAND} CARDID ${CARDID} FOLDER ${FOLDER} VALUE ${VALUE}" >> ${PATHDATA}/../logs/debug.log; fi
+			;;
+	esac
+
+fi
