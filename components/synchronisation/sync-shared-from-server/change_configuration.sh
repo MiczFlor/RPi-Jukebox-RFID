d32e65df73913c38427c26dd3b7a89348a91533b
AlvinSchiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-02-17 21:14:04 +0100

    changed definition of owner and rights in setup conf

--- components/synchronisation/sync-shared-from-server/change_configuration.sh
+++ components/synchronisation/sync-shared-from-server/change_configuration.sh
@@ -21,7 +21,8 @@ function set_activation {
 	echo -e "\nLet global controls know this feature is $SETTINGSTATE. (Sync_Shared_Enabled -> $SETTINGVALUE)"
 	
 	echo "$SETTINGVALUE" > ${CONFFILE}
-	chmod ugo+rw ${CONFFILE}
+	sudo chgrp www-data ${CONFFILE}
+	sudo chmod 775 ${CONFFILE}
 }
 
 function init_settings {
@@ -29,8 +30,8 @@ function init_settings {
 	if [ ! -f ${PROJROOTPATH}/settings/sync_shared.conf ]; then
 		cp ${PATHDATA}/settings/sync_shared.conf.sample ${PROJROOTPATH}/settings/sync_shared.conf
 		# change the read/write so that later this might also be editable through the web app
-		sudo chown -R pi:www-data ${PROJROOTPATH}/settings/sync_shared.conf
-		sudo chmod -R ugo+rw ${PROJROOTPATH}/settings/sync_shared.conf
+		sudo chgrp www-data ${PROJROOTPATH}/settings/sync_shared.conf
+		sudo chmod 775 ${PROJROOTPATH}/settings/sync_shared.conf
 	fi
 	. ${PROJROOTPATH}/settings/sync_shared.conf
 }
@@ -40,7 +41,7 @@ function set_setting {
 	local SETTINGVALUE="$2"
 
 	if [ ! -z "$SETTINGVALUE" -a "${!SETTINGNAME}" != "$SETTINGVALUE" ]; then
-		sed -i "s;^$SETTINGNAME=.*;$SETTINGNAME=\"$SETTINGVALUE\";" ${PROJROOTPATH}/settings/sync_shared.conf
+		sed -i "s|^$SETTINGNAME=.*|$SETTINGNAME=\"$SETTINGVALUE\"|g" ${PROJROOTPATH}/settings/sync_shared.conf
 		echo "New value: \"$SETTINGVALUE\""
 	fi
 }
