f9aacba6d11c98a92d2ff5aa4180e7b46ec5ce77
AlvinSchiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-02-19 03:57:08 +0100

    refactoring and path handling
    Make sure the path variables are alike and dont have a trailing slash
    moved command handling to funtions
    moved rsync to single function
    moved server check to funtion

--- components/synchronisation/sync-shared-from-server/change_configuration.sh
+++ components/synchronisation/sync-shared-from-server/change_configuration.sh
@@ -67,8 +67,9 @@ function read_all_settings {
 	read_setting "$SYNCSHAREDREMOTEPORT" "Please enter your servers port."
 	set_setting "SYNCSHAREDREMOTEPORT" "$response"
 
-	read_setting "$SYNCSHAREDREMOTEPATH" "Please enter the path to the shared files to sync."
-	set_setting "SYNCSHAREDREMOTEPATH" "$response"
+	read_setting "$SYNCSHAREDREMOTEPATH" "Please enter the path to the shared files to sync (without trailing slash)."
+	# Make sure paths dont have a trailing slash ({VAR%/})
+	set_setting "SYNCSHAREDREMOTEPATH" "${response%/}"
 
 	read_setting "$SYNCSHAREDREMOTETIMOUT" "Please enter the timeout to try to reach the server (in seconds)."
 	set_setting "SYNCSHAREDREMOTETIMOUT" "$response"
--- components/synchronisation/sync-shared-from-server/sync-shared.sh
+++ components/synchronisation/sync-shared-from-server/sync-shared.sh
@@ -31,7 +31,6 @@ NOW=`date +%Y-%m-%d.%H:%M:%S`
 # Unless you are working with symlinks, leave the following line untouched.
 PATHDATA="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJROOTPATH="$PATHDATA/../../.."
-SHORTCUTSPATH="$PROJROOTPATH/shared/shortcuts"
 
 #############################################################
 # $DEBUG TRUE|FALSE
@@ -57,7 +56,8 @@ else
 	#############################################################
 	# Read global configuration file (and create if not exists)
 	if [ ! -f ${PROJROOTPATH}/settings/global.conf ]; then
-		echo "Global settingsfile does not exist. Please call the script from an defined entrypoint"
+		echo "Global settingsfile does not exist. Please call the script from a defined entrypoint"
+		if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Global settingsfile does not exist. Please call the script from a defined entrypoint" >> ${PROJROOTPATH}/logs/debug.log; fi
 		exit
 	fi
 	. ${PROJROOTPATH}/settings/global.conf
@@ -66,6 +66,7 @@ else
 	# Read configuration file
 	if [ ! -f ${PROJROOTPATH}/settings/sync_shared.conf ]; then
 		echo "Settingsfile does not exist. Please read ${PATHDATA}/README.md to set configuration"
+		if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Settingsfile does not exist. Please read ${PATHDATA}/README.md to set configuration" >> ${PROJROOTPATH}/logs/debug.log; fi
 		exit
 	fi
 	. ${PROJROOTPATH}/settings/sync_shared.conf
@@ -82,14 +83,136 @@ else
 	if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "VAR VALUE: ${VALUE}" >> ${PROJROOTPATH}/logs/debug.log; fi
 	
 	#############################################################
-	# Set local vars after sync_shared.conf is read
+	# Set local vars after confs are read
+	# Make sure paths dont have a trailing slash ({VAR%/})
+	SYNCSHAREDREMOTEPATH="${SYNCSHAREDREMOTEPATH%/}"
 	SYNCSHORTCUTSPATH="${SYNCSHAREDREMOTEPATH}/shortcuts"
 	SYNCAUDIOFOLDERSPATH="${SYNCSHAREDREMOTEPATH}/audiofolders"
 	
+	LOCAL_SHORTCUTSPATH="$PROJROOTPATH/shared/shortcuts"
+	LOCAL_AUDIOFOLDERSPATH="${AUDIOFOLDERSPATH%/}"
 
 	#############################################################
 	#############################################################
 	# Functions
+	
+	# Check if server is reachable on port
+	function is_server_reachable {
+		return `nc -z $SYNCSHAREDREMOTESERVER -w $SYNCSHAREDREMOTETIMOUT $SYNCSHAREDREMOTEPORT`
+	}
+	
+	# Sync all files from source to destination
+	# Some special options are needed as the 'folder.conf' file will be generated on playback.
+	# Explanation for rsync options:
+	# "--itemize-changes" print a summary of copied files. Used for determination if files where changed (empty if no syncing performed). Useful for debug.log
+	# "--safe-links" ignore symlinks that point outside the tree
+	# "--times" preserve modification times from source. Recommended option to efficiently identify unchanged files
+	# "--omit-dir-times" ignore modification time on dirs (see --times). Needed to ignore the creation of 'folder.conf' which alters the modification time of dirs
+	# "--delete" delete files that no longer exist in source
+	# "--prune-empty-dirs" delete empty dirs (incl. subdirs)
+	# "--filter="-rp folder.conf" exclude (option '-') 'folder.conf' file from deletion on receiving side (option 'r'). Delete anyway if folder will be deleted (option 'p' (perishable)).
+	# "--exclude="placeholder" exclude 'placeholder' file from syncing, especially deletion
+	function sync_from_server {
+		local src_path="$1"
+		local dst_path="$2"
+		local update_mpc="$3"
+		
+		rsync_changes=$(rsync --compress --recursive --itemize-changes --safe-links --times --omit-dir-times --delete --prune-empty-dirs --filter='-rp folder.conf' --exclude='placeholder' $rsync_add_options "${src_path}" "${dst_path}")
+
+		if [ $? -eq 0 -a -n "${rsync_changes}" ]; then
+			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo -e "Sync: executed rsync \n${rsync_changes}" >> ${PROJROOTPATH}/logs/debug.log; fi
+			
+			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: files copied. change access of files" >> ${PROJROOTPATH}/logs/debug.log; fi
+			change_access "${dst_path}" "www-data" "775"
+			
+			if [ ! -z "${update_mpc}" ]; then
+				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: update database" >> ${PROJROOTPATH}/logs/debug.log; fi
+				sudo mpc update --wait "${dst_path}" > /dev/null 2>&1
+			fi
+			
+		else
+			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: nothing changed" >> ${PROJROOTPATH}/logs/debug.log; fi
+		fi
+	}
+	
+	# Sync shortcut CARDID
+	function handle_shortcuts {
+		if [ "${SYNCSHAREDONRFIDSCAN}" == "TRUE" ]; then 
+			if is_server_reachable ; then
+			
+				if [ ! -d "${SYNCSHORTCUTSPATH}" ]; then
+					mkdir "${SYNCSHORTCUTSPATH}"
+					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCSHORTCUTSPATH} does not exist. created" >> ${PROJROOTPATH}/logs/debug.log; fi
+				
+				elif [ -f "${SYNCSHORTCUTSPATH}/${CARDID}" ]; then
+					sync_from_server "${SYNCSHORTCUTSPATH}/${CARDID}" "${LOCAL_SHORTCUTSPATH}/${CARDID}"
+					
+				else 
+					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Shortcut for $CARDID not found in REMOTE $SYNCSHORTCUTSPATH" >> ${PROJROOTPATH}/logs/debug.log; fi
+				fi
+				
+			else 
+				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Server is NOT reachable" >> ${PROJROOTPATH}/logs/debug.log; fi
+			fi
+			
+		else 
+			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Sync on RFID scan deactivated" >> ${PROJROOTPATH}/logs/debug.log; fi
+		fi
+	}
+	
+	# Sync audiofolder FOLDER
+	function handle_audiofolders {
+		if [ "${SYNCSHAREDONRFIDSCAN}" == "TRUE" ]; then 
+			if is_server_reachable ; then
+			
+				if [ ! -d "${SYNCAUDIOFOLDERSPATH}" ]; then
+					mkdir "${SYNCAUDIOFOLDERSPATH}"
+					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCAUDIOFOLDERSPATH} does not exist. created" >> ${PROJROOTPATH}/logs/debug.log; fi
+				
+				elif [ -d "${SYNCAUDIOFOLDERSPATH}/${FOLDER}" ]; then
+					sync_from_server "${SYNCAUDIOFOLDERSPATH}/${FOLDER}/" "${LOCAL_AUDIOFOLDERSPATH}/${FOLDER}/" "true"
+					
+				else 
+					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder $FOLDER not found in REMOTE $SYNCAUDIOFOLDERSPATH" >> ${PROJROOTPATH}/logs/debug.log; fi
+				fi
+				
+			else 
+				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Server is NOT reachable" >> ${PROJROOTPATH}/logs/debug.log; fi
+			fi
+						
+		else 
+			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Sync on RFID scan deactivated" >> ${PROJROOTPATH}/logs/debug.log; fi
+		fi
+	}
+	
+	# Sync full (shortcuts and audiofolders)
+	function handle_full {
+		if is_server_reachable ; then
+		
+			if [ ! -d "${SYNCSHORTCUTSPATH}" ]; then
+				mkdir "${SYNCSHORTCUTSPATH}"
+				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCSHORTCUTSPATH} does not exist. created" >> ${PROJROOTPATH}/logs/debug.log; fi
+			else
+				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCSHORTCUTSPATH}" >> ${PROJROOTPATH}/logs/debug.log; fi
+				sync_from_server "${SYNCSHORTCUTSPATH}/" "${LOCAL_SHORTCUTSPATH}/"
+
+			fi
+				
+			if [ ! -d "${SYNCAUDIOFOLDERSPATH}" ]; then
+				mkdir "${SYNCAUDIOFOLDERSPATH}"
+				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCAUDIOFOLDERSPATH} does not exist. created" >> ${PROJROOTPATH}/logs/debug.log; fi
+			else
+				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCAUDIOFOLDERSPATH}" >> ${PROJROOTPATH}/logs/debug.log; fi
+				sync_from_server "${SYNCAUDIOFOLDERSPATH}/" "${LOCAL_AUDIOFOLDERSPATH}/" "true"
+				
+			fi
+			
+		else 
+			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Server is NOT reachable" >> ${PROJROOTPATH}/logs/debug.log; fi
+		fi
+	}
+	
+	# Change setting for Sync on RFID scan
 	function handle_changeOnRfidScan {
 		case $VALUE in
 			on)
@@ -112,11 +235,12 @@ else
 		esac
 
 		if [ ! -z "${SYNCSHAREDONRFIDSCAN_NEW}" ]; then 
-			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Set SYNCSHAREDONRFIDSCAN to $VALUE" >> ${PROJROOTPATH}/logs/debug.log; fi
+			if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Set SYNCSHAREDONRFIDSCAN to $SYNCSHAREDONRFIDSCAN_NEW" >> ${PROJROOTPATH}/logs/debug.log; fi
 			sed -i "s|^SYNCSHAREDONRFIDSCAN=.*|SYNCSHAREDONRFIDSCAN=\"$SYNCSHAREDONRFIDSCAN_NEW\"|g" ${PROJROOTPATH}/settings/sync_shared.conf
 		fi
 	}
 	
+	# Change access of file or dir
 	function change_access {
 		local file_or_dir="$1"
 		local group="$2"
@@ -130,131 +254,15 @@ else
 	#############################################################
 	
 	#############################################################
-	# Sync all files for the specific command. 
-	# Some special options are needed as the 'folder.conf' file will be generated on playback.
-	# Explanation for rsync options:
-	# "--itemize-changes" print a summary of copied files. Used for determination if files where changed (empty if no syncing performed). Useful for debug.log
-	# "--safe-links" ignore symlinks that point outside the tree
-	# "--times" preserve modification times from source. Recommended option to efficiently identify unchanged files
-	# "--omit-dir-times" ignore modification time on dirs (see --times). Needed to ignore the creation of 'folder.conf' which alters the modification time of dirs
-	# "--update" ignore newer files on destination. Lower traffic and runtime
-	# "--delete" delete files that no longer exist in source
-	# "--prune-empty-dirs" delete empty dirs (incl. subdirs)
-	# "--filter="-rp folder.conf" exclude (option '-') 'folder.conf' file from deletion on receiving side (option 'r'). Delete anyway if folder will be deleted (option 'p' (perishable)).
-	# "--exclude="placeholder" exclude 'placeholder' file from syncing, especially deletion
 	case $COMMAND in
 		shortcuts)
-			if [ "${SYNCSHAREDONRFIDSCAN}" == "TRUE" ]; then 
-				if nc -z $SYNCSHAREDREMOTESERVER -w $SYNCSHAREDREMOTETIMOUT $SYNCSHAREDREMOTEPORT ; then
-				
-					if [ ! -d "${SYNCSHORTCUTSPATH}" ]; then
-						mkdir "${SYNCSHORTCUTSPATH}"
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCSHORTCUTSPATH} does not exist. created" >> ${PROJROOTPATH}/logs/debug.log; fi
-					
-					elif [ -f "${SYNCSHORTCUTSPATH}/${CARDID}" ]; then
-						RSYNCSHORTCUTSCMD=$(rsync --compress --recursive --itemize-changes --safe-links --times --omit-dir-times "${SYNCSHORTCUTSPATH}/${CARDID}" "${SHORTCUTSPATH}/")
-						
-						if [ $? -eq 0 -a -n "${RSYNCSHORTCUTSCMD}" ]; then
-							if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: executed rsync ${RSYNCSHORTCUTSCMD}" >> ${PROJROOTPATH}/logs/debug.log; fi
-							if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: files copied. setting rights" >> ${PROJROOTPATH}/logs/debug.log; fi
-							
-							change_access "${SHORTCUTSPATH}/${CARDID}" "www-data" "775"
-							
-						else
-							if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: nothing changed" >> ${PROJROOTPATH}/logs/debug.log; fi
-						fi
-						
-					else 
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Shortcut for $CARDID not found in REMOTE $SYNCSHORTCUTSPATH" >> ${PROJROOTPATH}/logs/debug.log; fi
-					fi
-					
-				else 
-					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Server is NOT reachable" >> ${PROJROOTPATH}/logs/debug.log; fi
-				fi
-				
-			else 
-				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Sync on RFID scan deactivated" >> ${PROJROOTPATH}/logs/debug.log; fi
-			fi
+			handle_shortcuts
 			;;
 		audiofolders)
-			if [ "${SYNCSHAREDONRFIDSCAN}" == "TRUE" ]; then 
-				if nc -z $SYNCSHAREDREMOTESERVER -w $SYNCSHAREDREMOTETIMOUT $SYNCSHAREDREMOTEPORT ; then
-				
-					if [ ! -d "${SYNCAUDIOFOLDERSPATH}" ]; then
-						mkdir "${SYNCAUDIOFOLDERSPATH}"
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCAUDIOFOLDERSPATH} does not exist. created" >> ${PROJROOTPATH}/logs/debug.log; fi
-					
-					elif [ -d "${SYNCAUDIOFOLDERSPATH}/${FOLDER}" ]; then
-						RSYNCSAUDIOFILES=$(rsync --compress --recursive --itemize-changes --safe-links --times --omit-dir-times --update --delete --prune-empty-dirs --filter="-rp folder.conf" "${SYNCAUDIOFOLDERSPATH}/${FOLDER}/" "${AUDIOFOLDERSPATH}/${FOLDER}/")
-						
-						if [ $? -eq 0 -a -n "${RSYNCSAUDIOFILES}" ]; then
-							if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: executed rsync ${RSYNCSAUDIOFILES}" >> ${PROJROOTPATH}/logs/debug.log; fi
-							if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: files copied. setting rights and update database" >> ${PROJROOTPATH}/logs/debug.log; fi
-							
-							change_access "${AUDIOFOLDERSPATH}/${FOLDER}" "www-data" "775"
-							sudo mpc update --wait "${AUDIOFOLDERSPATH}/${FOLDER}" > /dev/null 2>&1
-							
-						else
-							if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: nothing changed" >> ${PROJROOTPATH}/logs/debug.log; fi
-						fi
-						
-					else 
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder $FOLDER not found in REMOTE $SYNCAUDIOFOLDERSPATH" >> ${PROJROOTPATH}/logs/debug.log; fi
-					fi
-					
-				else 
-					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Server is NOT reachable" >> ${PROJROOTPATH}/logs/debug.log; fi
-				fi
-							
-			else 
-				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Sync on RFID scan deactivated" >> ${PROJROOTPATH}/logs/debug.log; fi
-			fi
+			handle_audiofolders
 			;;
 		full)
-			if nc -z $SYNCSHAREDREMOTESERVER -w $SYNCSHAREDREMOTETIMOUT $SYNCSHAREDREMOTEPORT ; then
-			
-				if [ ! -d "${SYNCSHORTCUTSPATH}" ]; then
-					mkdir "${SYNCSHORTCUTSPATH}"
-					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCSHORTCUTSPATH} does not exist. created" >> ${PROJROOTPATH}/logs/debug.log; fi
-				else
-					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCSHORTCUTSPATH}" >> ${PROJROOTPATH}/logs/debug.log; fi
-					RSYNCSHORTCUTSCMD=$(rsync --compress --recursive --itemize-changes --safe-links --times --omit-dir-times --delete --prune-empty-dirs --exclude="placeholder" "${SYNCSHORTCUTSPATH}/" "${SHORTCUTSPATH}/")
-					
-					if [ $? -eq 0 -a -n "${RSYNCSHORTCUTSCMD}" ]; then
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: executed rsync ${RSYNCSHORTCUTSCMD}" >> ${PROJROOTPATH}/logs/debug.log; fi
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: files copied. setting rights" >> ${PROJROOTPATH}/logs/debug.log; fi
-						
-						change_access "${SHORTCUTSPATH}" "www-data" "775"
-						
-					else
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: nothing changed" >> ${PROJROOTPATH}/logs/debug.log; fi
-					fi
-
-				fi
-					
-				if [ ! -d "${SYNCAUDIOFOLDERSPATH}" ]; then
-					mkdir "${SYNCAUDIOFOLDERSPATH}"
-					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCAUDIOFOLDERSPATH} does not exist. created" >> ${PROJROOTPATH}/logs/debug.log; fi
-				else
-					if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Folder ${SYNCAUDIOFOLDERSPATH}" >> ${PROJROOTPATH}/logs/debug.log; fi
-					RSYNCSAUDIOFILES=$(rsync --compress --recursive --itemize-changes --safe-links --times --omit-dir-times --update --delete --prune-empty-dirs --filter="-rp folder.conf" --exclude="placeholder" "${SYNCAUDIOFOLDERSPATH}/" "${AUDIOFOLDERSPATH}/")
-					
-					if [ $? -eq 0 -a -n "${RSYNCSAUDIOFILES}" ]; then
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: executed rsync ${RSYNCSAUDIOFILES}" >> ${PROJROOTPATH}/logs/debug.log; fi
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: files copied. setting rights and update database" >> ${PROJROOTPATH}/logs/debug.log; fi
-						
-						change_access "${AUDIOFOLDERSPATH}" "www-data" "775"
-						sudo mpc update --wait "${AUDIOFOLDERSPATH}" > /dev/null 2>&1
-						
-					else
-						if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: nothing changed" >> ${PROJROOTPATH}/logs/debug.log; fi
-					fi
-					
-				fi
-				
-			else 
-				if [ "${DEBUG_sync_shared_sh}" == "TRUE" ]; then echo "Sync: Server is NOT reachable" >> ${PROJROOTPATH}/logs/debug.log; fi
-			fi
+			handle_full
 			;;
 		changeOnRfidScan)
 			handle_changeOnRfidScan
