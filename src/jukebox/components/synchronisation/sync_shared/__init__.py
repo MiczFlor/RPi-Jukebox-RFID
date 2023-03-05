8ac52de892dc31eb8c17c673d55e8b9b7ef1dd0c
AlvinSchiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-03-06 00:58:45 +0100

    moved test callback methods

--- src/jukebox/components/synchronisation/sync_shared/__init__.py
+++ src/jukebox/components/synchronisation/sync_shared/__init__.py
@@ -2,12 +2,12 @@ import logging
 import jukebox.cfghandler
 import jukebox.plugs as plugin
 import components.rfid.reader
-import components.synchronisation.sync_shared as sync_shared
 
 logger = logging.getLogger('jb.synchronisation.sync_shared')
 
 cfg_main = jukebox.cfghandler.get_handler('jukebox')
 cfg_sync_shared = jukebox.cfghandler.get_handler('sync_shared')
+
 
 @plugin.initialize
 def initialize():
@@ -19,4 +19,16 @@ def initialize():
             logger.error(f"Error loading sync_shared config file. {e.__class__.__name__}: {e}")
             return
 
-        components.rfid.reader.rfid_card_detect_callbacks.register(sync_shared.rfid_card_detect_callback)
+        components.rfid.reader.rfid_card_detect_callbacks.register(rfid_card_detect_callback)
+
+def syncCardDatabase():
+    logger("Sync Database.")
+
+
+def rfid_card_detect_callback(card_id: str, state: int):
+    logger("RFID Scan Callback.")
+    if (state != 0):
+        logger("Unkown CardId. No syncing")
+    else:
+        logger(f"CardId {card_id}. syncing")
+
