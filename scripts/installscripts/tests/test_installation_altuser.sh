7581a872f986d361ceff4c4f77328e44a3f1a7aa
s-martin <s-martin@users.noreply.github.com>
2023-03-26 16:46:05 +0200

    fix also test

--- scripts/installscripts/tests/test_installation_altuser.sh
+++ scripts/installscripts/tests/test_installation_altuser.sh
@@ -152,12 +152,12 @@ verify_wifi_settings() {
 }
 
 verify_apt_packages(){
-    local packages="libspotify-dev samba
+    local packages="samba
 samba-common-bin gcc lighttpd php7.3-common php7.3-cgi php7.3 at mpd mpc mpg123 git ffmpeg
 resolvconf spi-tools python3 python3-dev python3-pip python3-mutagen python3-gpiozero
 python3-spidev netcat alsa-utils"
     local packages_raspberrypi="raspberrypi-kernel-headers"
-    local packages_spotify="mopidy mopidy-mpd mopidy-local mopidy-spotify libspotify12
+    local packages_spotify="libspotify-dev mopidy mopidy-mpd mopidy-local mopidy-spotify libspotify12
 python3-cffi python3-ply python3-pycparser python3-spotify"
 
     printf "\nTESTING installed packages...\n\n"
