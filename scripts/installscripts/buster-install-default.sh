41f8c2c2a601c7ca1f8e12f08c99f3486b212db3
s-martin <s-martin@users.noreply.github.com>
2023-04-02 19:36:51 +0000

    dont handle resolv.conf on docker test envs

--- scripts/installscripts/buster-install-default.sh
+++ scripts/installscripts/buster-install-default.sh
@@ -782,8 +782,11 @@ install_main() {
     # power management of wifi: switch off to avoid disconnecting
     sudo iwconfig wlan0 power off
 
-    # create backup of /etc/resolv.conf
-    sudo cp /etc/resolv.conf /etc/resolv.conf.orig
+    # in the docker test env fiddling with resolv.conf causes issues, see https://stackoverflow.com/a/60576223
+    if [ "$DOCKER_RUNNING" != "true" ]; then
+        # create backup of /etc/resolv.conf
+        sudo cp /etc/resolv.conf /etc/resolv.conf.orig
+    fi
 
     # Generate locales
     sudo locale-gen "${LANG}"
@@ -801,8 +804,11 @@ install_main() {
 
     ${apt_get} ${allow_downgrades} install samba samba-common-bin gcc lighttpd php-common php-cgi php at mpd mpc mpg123 git ffmpeg resolvconf spi-tools netcat alsa-utils lsof procps
 
-    # restore backup of /etc/resolv.conf in case installation of resolvconf cleared it
-    sudo cp /etc/resolv.conf.orig /etc/resolv.conf
+    # in the docker test env fiddling with resolv.conf causes issues, see https://stackoverflow.com/a/60576223
+    if [ "$DOCKER_RUNNING" != "true" ]; then
+        # restore backup of /etc/resolv.conf in case installation of resolvconf cleared it
+        sudo cp /etc/resolv.conf.orig /etc/resolv.conf
+    fi
 
     # prepare python3
     ${apt_get} ${allow_downgrades} install python3 python3-dev python3-pip python3-setuptools python3-wheel python3-mutagen python3-gpiozero python3-spidev
