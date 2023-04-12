b963ef9cbcb8fa151991b5650a297d39e57b90ee
AlvinSchiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-04-12 14:21:24 +0200

    fixed flake8

--- src/jukebox/components/synchronisation/sync_shared/syncutils.py
+++ src/jukebox/components/synchronisation/sync_shared/syncutils.py
@@ -3,15 +3,18 @@ def clean_foldername(lib_path: str, folder: str) -> str:
     _folder = remove_leading_slash(remove_trailing_slash(_folder))
     return _folder
 
+
 def ensure_trailing_slash(path: str) -> str:
     _path = path
     if not _path.endswith('/'):
         _path = _path + '/'
     return _path
 
+
 def remove_trailing_slash(path: str) -> str:
     _path = path.removesuffix('/')
     return _path
+
 
 def remove_leading_slash(path: str) -> str:
     _path = path.removeprefix('/')
