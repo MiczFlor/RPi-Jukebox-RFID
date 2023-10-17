cf564d2da24bf2f970a71d58305c40840332a40f
s-martin <s-martin@users.noreply.github.com>
2023-10-17 11:13:48 +0200

    explicitly install netcat-traditional (necessary since Debian bookworm)

--- scripts/installscripts/buster-install-default.sh
+++ scripts/installscripts/buster-install-default.sh
@@ -824,7 +824,7 @@ install_main() {
         ${apt_get} ${allow_downgrades} install raspberrypi-kernel-headers
     fi
 
-    ${apt_get} ${allow_downgrades} install samba samba-common-bin gcc lighttpd php-common php-cgi php at mpd mpc mpg123 git ffmpeg resolvconf spi-tools netcat alsa-utils lsof procps
+    ${apt_get} ${allow_downgrades} install samba samba-common-bin gcc lighttpd php-common php-cgi php at mpd mpc mpg123 git ffmpeg resolvconf spi-tools netcat-traditional alsa-utils lsof procps
 
     # in the docker test env fiddling with resolv.conf causes issues, see https://stackoverflow.com/a/60576223
     if [ "$DOCKER_RUNNING" != "true" ]; then
