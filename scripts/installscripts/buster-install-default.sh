c13907a9ad1d4d75f6f71daa71674fe811268caf
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-10-31 22:43:16 +0100

    fixes for "clear autohotspot configurations"

--- scripts/installscripts/buster-install-default.sh
+++ scripts/installscripts/buster-install-default.sh
@@ -1314,7 +1314,10 @@ autohotspot() {
     local jukebox_dir="$1"
     local apt_get="sudo apt-get -qq --yes"
 
+    local systemd_dir="/etc/systemd/system"
+
     local autohotspot_service="autohotspot.service"
+    local autohotspot_service_path="${systemd_dir}/${autohotspot_service}"
     local autohotspot_script="/usr/bin/autohotspot"
 
     local dnsmasq_conf=/etc/dnsmasq.conf
@@ -1337,8 +1340,12 @@ autohotspot() {
         sudo systemctl disable dnsmasq
 
         # configure DNS
-        if [[ -f $dnsmasq_conf && ! -f "$dnsmasq_conf".orig ]]; then
-            sudo cp $dnsmasq_conf "$dnsmasq_conf".orig
+        # create flag file or copy present conf to orig file
+        # to correctly handling future deactivation of autohotspot
+        if [ ! -f "${dnsmasq_conf}" ]; then
+            sudo touch "${dnsmasq_conf}.remove"
+        elif [ ! -f "${dnsmasq_conf}.orig" ]; then
+            sudo cp "${dnsmasq_conf}" "${dnsmasq_conf}.orig"
         fi
 
         local ip_without_last_segment=$(echo $AUTOHOTSPOTip | cut -d'.' -f1-3)
@@ -1348,8 +1355,12 @@ autohotspot() {
         sudo chmod 644 "${dnsmasq_conf}"
 
         # configure hostapd conf
-        if [[ -f $hostapd_conf && ! -f "$hostapd_conf".orig ]]; then
-            sudo cp $hostapd_conf "$hostapd_conf".orig
+        # create flag file or copy present conf to orig file
+        # to correctly handling future deactivation of autohotspot
+        if [ ! -f "${hostapd_conf}" ]; then
+            sudo touch "${hostapd_conf}.remove"
+        elif [ ! -f "${hostapd_conf}.orig" ]; then
+            sudo cp "${hostapd_conf}" "${hostapd_conf}.orig"
         fi
 
         sudo cp "${jukebox_dir}"/misc/sampleconfigs/hostapd.conf.stretch-default2-Hotspot.sample "${hostapd_conf}"
@@ -1360,8 +1371,12 @@ autohotspot() {
         sudo chmod 644 "${hostapd_conf}"
 
         # configure hostapd daemon
-        if [[ -f $hostapd_deamon && ! -f "$hostapd_deamon".orig ]]; then
-            sudo cp $hostapd_deamon "$hostapd_deamon".orig
+        # create flag file or copy present conf to orig file
+        # to correctly handling future deactivation of autohotspot
+        if [ ! -f "${hostapd_deamon}" ]; then
+            sudo touch "${hostapd_deamon}.remove"
+        elif [ ! -f "${hostapd_deamon}.orig" ]; then
+            sudo cp "${hostapd_deamon}" "${hostapd_deamon}.orig"
         fi
 
         sudo cp "${jukebox_dir}"/misc/sampleconfigs/hostapd.stretch-default2-Hotspot.sample "${hostapd_deamon}"
@@ -1370,12 +1385,19 @@ autohotspot() {
         sudo chmod 644 "${hostapd_deamon}"
 
         # configure dhcpcd conf
-        if [ ! -f $dhcpcd_conf ]; then
-            sudo touch $dhcpcd_conf
+        # create flag file or copy present conf to orig file
+        # to correctly handling future deactivation of autohotspot
+        if [ ! -f "${dhcpcd_conf}" ]; then
+            sudo touch "${dhcpcd_conf}.remove"
+            sudo touch "${dhcpcd_conf}"
+            sudo chown root:netdev "${dhcpcd_conf}"
+            sudo chmod 664 "${dhcpcd_conf}"
+        elif [ ! -f "${dhcpcd_conf}.orig" ]; then
+            sudo cp "${dhcpcd_conf}" "${dhcpcd_conf}.orig"
         fi
 
-        if [[ ! $(grep -w "${dhcpcd_conf_nohook_wpa_supplicant}" $dhcpcd_conf) ]]; then
-            sudo bash -c "echo ${dhcpcd_conf_nohook_wpa_supplicant} >> $dhcpcd_conf"
+        if [[ ! $(grep -w "${dhcpcd_conf_nohook_wpa_supplicant}" ${dhcpcd_conf}) ]]; then
+            sudo bash -c "echo ${dhcpcd_conf_nohook_wpa_supplicant} >> ${dhcpcd_conf}"
         fi
 
         # create service to trigger hotspot
@@ -1383,7 +1405,6 @@ autohotspot() {
         sudo sed -i "s|10.0.0.5|${AUTOHOTSPOTip}|g" "${autohotspot_script}"
         sudo chmod +x "${autohotspot_script}"
 
-        local autohotspot_service_path="/etc/systemd/system/${autohotspot_service}"
         sudo cp "${jukebox_dir}"/misc/sampleconfigs/autohotspot.service.stretch-default2-Hotspot.sample "${autohotspot_service_path}"
         sudo sed -i "s|%AUTOHOTSPOT_SCRIPT%|${autohotspot_script}|g" "${autohotspot_service_path}"
         sudo chown root:root "${autohotspot_service_path}"
@@ -1393,51 +1414,52 @@ autohotspot() {
 
         # create crontab entry
         local crontab_user=$(crontab -l 2>/dev/null)
-        if [[ -z "$crontab_user" || ! $(echo "$crontab_user" | grep -w "${autohotspot_script}") ]]; then
-            (echo "$crontab_user"; echo "*/5 * * * * sudo ${autohotspot_script} >/dev/null 2>&1") | crontab -
+        if [[ -z "${crontab_user}" || ! $(echo "${crontab_user}" | grep -w "${autohotspot_script}") ]]; then
+            (echo "${crontab_user}"; echo "*/5 * * * * sudo ${autohotspot_script} >/dev/null 2>&1") | crontab -
         fi
 
     else
-        # disable services if autohotspot option was not selected
-        if systemctl list-unit-files hostapd.service >/dev/null 2>&1 ; then
+        # clear autohotspot configurations made from past installation
+
+        # stop services and clear services
+        if systemctl list-unit-files "${autohotspot_service}" >/dev/null 2>&1 ; then
             sudo systemctl stop hostapd
-            sudo systemctl disable hostapd
-        fi
-        if systemctl list-unit-files dnsmasq.service >/dev/null 2>&1 ; then
             sudo systemctl stop dnsmasq
-            sudo systemctl disable dnsmasq
-        fi
-        if systemctl list-unit-files ${autohotspot_service} >/dev/null 2>&1 ; then
             sudo systemctl stop "${autohotspot_service}"
             sudo systemctl disable "${autohotspot_service}"
-            sudo rm ${autohotspot_service_path}
+            sudo rm "${autohotspot_service_path}"
         fi
 
+        # remove crontab entry and script
         local crontab_user=$(crontab -l 2>/dev/null)
-        if [[ ! -z "$crontab_user" && ! $(echo "$crontab_user" | grep -w "${autohotspot_script}") ]]; then
-            echo "$crontab_user" | sed "s|^.*\s${autohotspot_script}\s.*$||g" | crontab -
-        fi
-        if [ -f $autohotspot_script ]; then
-            sudo rm $autohotspot_script
+        if [[ ! -z "${crontab_user}" && ! $(echo "${crontab_user}" | grep -w "${autohotspot_script}") ]]; then
+            echo "${crontab_user}" | sed "s|^.*\s${autohotspot_script}\s.*$||g" | crontab -
         fi
 
-        if [[ -f "$dnsmasq_conf".orig ]]; then
-            sudo cp "$dnsmasq_conf".orig "$dnsmasq_conf"
-        else
-            sudo rm "$dnsmasq_conf"
+        if [ -f "${autohotspot_script}" ]; then
+            sudo rm "${autohotspot_script}"
         fi
-        if [[ -f "$hostapd_conf".orig ]]; then
-            sudo cp "$hostapd_conf".orig "$hostapd_conf"
-        else
-            sudo rm "$hostapd_conf"
+
+        # remove config files
+        if [ -f "${dnsmasq_conf}.remove" ]; then
+            sudo rm "${dnsmasq_conf}.remove" "${dnsmasq_conf}"
+        elif [ -f "${dnsmasq_conf}.orig" ]; then
+            sudo mv "${dnsmasq_conf}.orig" "${dnsmasq_conf}"
         fi
-        if [[ -f "$hostapd_deamon".orig ]]; then
-            sudo cp "$hostapd_deamon".orig "$hostapd_deamon"
-        else
-            sudo rm "$hostapd_deamon"
+        if [ -f "${hostapd_conf}.remove" ]; then
+            sudo rm "${hostapd_conf}.remove" "${hostapd_conf}"
+        elif [ -f "${hostapd_conf}.orig" ]; then
+            sudo mv "${hostapd_conf}.orig" "${hostapd_conf}"
         fi
-        if [[ -f $dhcpcd_conf && $(grep -w "${dhcpcd_conf_nohook_wpa_supplicant}" $dhcpcd_conf) ]]; then
-            sudo sed -i "s|${dhcpcd_conf_nohook_wpa_supplicant}||g" "${dhcpcd_conf}"
+        if [ -f "${hostapd_deamon}.remove" ]; then
+            sudo rm "${hostapd_deamon}.remove" "${hostapd_deamon}"
+        elif [ -f "${hostapd_deamon}.orig" ]; then
+            sudo mv "${hostapd_deamon}.orig" "${hostapd_deamon}"
+        fi
+        if [ -f "${dhcpcd_conf}.remove" ]; then
+            sudo rm "${dhcpcd_conf}.remove" "${dhcpcd_conf}"
+        elif [ -f "${dhcpcd_conf}.orig" ]; then
+            sudo mv "${dhcpcd_conf}.orig" "${dhcpcd_conf}"
         fi
     fi
 }
