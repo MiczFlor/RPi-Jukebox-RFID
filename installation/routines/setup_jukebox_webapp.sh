bab3483a144c29c864f2b8938e753c1c0bb2d8de
pabera <1260686+pabera@users.noreply.github.com>
2023-11-12 20:48:28 +0100

    Potentially fix node installation

--- installation/routines/setup_jukebox_webapp.sh
+++ installation/routines/setup_jukebox_webapp.sh
@@ -33,6 +33,8 @@ _jukebox_webapp_install_node() {
       # install NodeJS and npm as recommended in
       # https://github.com/nodesource/distributions
       curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+      sudo apt-get install -y ca-certificates curl gnupg
+      sudo mkdir -p /etc/apt/keyrings
       echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
       sudo apt-get update
       sudo apt-get install nodejs npm -y
