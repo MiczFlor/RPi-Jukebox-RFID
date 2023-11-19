616f859f46e77b9ca72098d44ed0cbec04c8b94a
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-11-19 03:41:14 +0100

    add check for samba setup

--- installation/routines/setup_samba.sh
+++ installation/routines/setup_samba.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+SMB_CONF="/etc/samba/smb.conf"
+SMB_CONF_HEADER="## Jukebox Samba Config"
+
 _samba_install_os_dependencies() {
   echo "Install Samba Core dependencies"
   sudo apt-get -qq -y update; sudo apt-get -qq -y install \
@@ -11,12 +14,11 @@ _samba_install_os_dependencies() {
 }
 
 _samba_set_user() {
-  local SMB_CONF="/etc/samba/smb.conf"
   local SMB_USER="pi"
   local SMB_PASSWD="raspberry"
 
   # Samba has not been configured
-  if grep -q "## Jukebox Samba Config" "$SMB_CONF"; then
+  if grep -q "$SMB_CONF_HEADER" "$SMB_CONF"; then
     echo "  Skipping. Already set up!" | tee /dev/fd/3
   else
     # Create Samba user
@@ -27,7 +29,7 @@ _samba_set_user() {
 
     # Create Samba Mount Points
     sudo cat << EOF >> $SMB_CONF
-## Jukebox Samba Config
+${SMB_CONF_HEADER}
 [phoniebox]
   comment= Pi Jukebox
   path=${SHARED_PATH}
@@ -43,6 +45,16 @@ EOF
   fi
 }
 
+_samba_check () {
+    echo "Check Samba Installation" | tee /dev/fd/3
+    verify_apt_packages samba samba-common-bin
+
+    verify_files_chmod_chown 644 root root "${SMB_CONF}"
+
+    verify_file_contains_string "${SMB_CONF_HEADER}" "${SMB_CONF}"
+    verify_file_contains_string "${SHARED_PATH}" "${SMB_CONF}"
+}
+
 setup_samba() {
   echo "Install Samba and configure user" | tee /dev/fd/3
 
@@ -50,6 +62,7 @@ setup_samba() {
   echo "samba-common samba-common/dhcp boolean false" | sudo debconf-set-selections
   _samba_install_os_dependencies
   _samba_set_user
+  _samba_check
 
   echo "DONE: setup_samba"
 }
