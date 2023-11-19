fec3c34905b52d48a5e1472d8a6e3c61537eea56
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-11-19 03:54:15 +0100

    add check for optimize boot setup

--- installation/includes/00_constants.sh
+++ installation/includes/00_constants.sh
@@ -1,4 +1,5 @@
 RPI_BOOT_CONFIG_FILE="/boot/config.txt"
+RPI_BOOT_CMDLINE_FILE="/boot/cmdline.txt"
 SHARED_PATH="${INSTALLATION_PATH}/shared"
 SETTINGS_PATH="${SHARED_PATH}/settings"
 SYSTEMD_USR_PATH="/usr/lib/systemd/user"
--- installation/routines/optimize_boot_time.sh
+++ installation/routines/optimize_boot_time.sh
@@ -2,6 +2,12 @@
 
 # Reference: https://panther.software/configuration-code/raspberry-pi-3-4-faster-boot-time-in-few-easy-steps/
 
+OPTIMIZE_DHCP_CONF="/etc/dhcpcd.conf"
+OPTIMIZE_BOOT_CMDLINE_OPTIONS="consoleblank=1 logo.nologo quiet loglevel=0 plymouth.enable=0 vt.global_cursor_default=0 plymouth.ignore-serial-consoles splash fastboot noatime nodiratime noram"
+OPTIMIZE_DHCP_CONF_HEADER="## Jukebox DHCP Config"
+OPTIMIZE_IPV6_CONF_HEADER="## Jukebox IPV6 Config"
+OPTIMIZE_BOOT_CONF_HEADER="## Jukebox Boot Config"
+
 _optimize_disable_irrelevant_services() {
   echo "  * Disable keyboard-setup.service"
   sudo systemctl disable keyboard-setup.service
@@ -32,11 +38,9 @@ _optimize_handle_bluetooth() {
 # TODO: Allow options to enable/disable wifi, Dynamic/Static IP etc.
 _optimize_handle_network_connection() {
   # Static IP Address and DHCP optimizations
-  local DHCP_CONF="/etc/dhcpcd.conf"
-
   if [ "$ENABLE_STATIC_IP" = true ] ; then
     echo "  * Set static IP address" | tee /dev/fd/3
-    if grep -q "## Jukebox DHCP Config" "$DHCP_CONF"; then
+    if grep -q "${OPTIMIZE_DHCP_CONF_HEADER}" "$OPTIMIZE_DHCP_CONF"; then
       echo "    Skipping. Already set up!" | tee /dev/fd/3
     else
       # DHCP has not been configured
@@ -44,9 +48,9 @@ _optimize_handle_network_connection() {
       echo "    * ${CURRENT_GATEWAY} is the Router Gateway address" | tee /dev/fd/3
       echo "    * Using ${CURRENT_IP_ADDRESS} as the static IP for now" | tee /dev/fd/3
 
-      sudo tee -a $DHCP_CONF <<-EOF
+      sudo tee -a $OPTIMIZE_DHCP_CONF <<-EOF
 
-## Jukebox DHCP Config
+${OPTIMIZE_DHCP_CONF_HEADER}
 interface ${CURRENT_INTERFACE}
 static ip_address=${CURRENT_IP_ADDRESS}/24
 static routers=${CURRENT_GATEWAY}
@@ -63,16 +67,19 @@ EOF
 # TODO: Allow both Enable and Disable
 _optimize_ipv6_arp() {
   if [ "$DISABLE_IPv6" = true ] ; then
-      echo "  * Disabling IPV6 and ARP"
-      sudo tee -a $DHCP_CONF <<-EOF
+    echo "  * Disabling IPV6 and ARP"
+    if grep -q "${OPTIMIZE_IPV6_CONF_HEADER}" "$OPTIMIZE_DHCP_CONF"; then
+      echo "    Skipping. Already set up!" | tee /dev/fd/3
+    else
+      sudo tee -a $OPTIMIZE_DHCP_CONF <<-EOF
 
-## Jukebox boot speed-up settings
+${OPTIMIZE_IPV6_CONF_HEADER}
 noarp
 ipv4only
 noipv6
 
 EOF
-
+    fi
   fi
 }
 
@@ -80,13 +87,16 @@ EOF
 _optimize_handle_boot_screen() {
   if [ "$DISABLE_BOOT_SCREEN" = true ] ; then
     echo "  * Disable RPi rainbow screen"
-    BOOT_CONFIG='/boot/config.txt'
-    sudo tee -a $BOOT_CONFIG <<-EOF
+    if grep -q "${OPTIMIZE_BOOT_CONF_HEADER}" "$RPI_BOOT_CONFIG_FILE"; then
+      echo "    Skipping. Already set up!" | tee /dev/fd/3
+    else
+      sudo tee -a $RPI_BOOT_CONFIG_FILE <<-EOF
 
-## Jukebox Settings
+${OPTIMIZE_BOOT_CONF_HEADER}
 disable_splash=1
 
 EOF
+    fi
   fi
 }
 
@@ -94,9 +104,56 @@ EOF
 _optimize_handle_boot_logs() {
   if [ "$DISABLE_BOOT_LOGS_PRINT" = true ] ; then
     echo "  * Disable boot logs"
-    BOOT_CMDLINE='/boot/cmdline.txt'
-    sudo sed -i "$ s/$/ consoleblank=1 logo.nologo quiet loglevel=0 plymouth.enable=0 vt.global_cursor_default=0 plymouth.ignore-serial-consoles splash fastboot noatime nodiratime noram/" $BOOT_CMDLINE
+
+    if [ ! -s "${RPI_BOOT_CMDLINE_FILE}" ];then
+        sudo tee $RPI_BOOT_CMDLINE_FILE <<-EOF
+${OPTIMIZE_BOOT_CMDLINE_OPTIONS}
+EOF
+    else
+        for option in $OPTIMIZE_BOOT_CMDLINE_OPTIONS
+        do
+            if ! $(grep -iw "$option" "${RPI_BOOT_CMDLINE_FILE}") ; then
+                sudo sed -i "s/$/ $option/" "${RPI_BOOT_CMDLINE_FILE}"
+            fi
+        done
+    fi
   fi
+}
+
+
+_optimize_check () {
+    echo "Check Optimize boot time" | tee /dev/fd/3
+
+    check_optional_service_enablement keyboard-setup.service disabled
+    check_optional_service_enablement triggerhappy.service disabled
+    check_optional_service_enablement triggerhappy.socket disabled
+    check_optional_service_enablement raspi-config.service disabled
+    check_optional_service_enablement apt-daily.service disabled
+    check_optional_service_enablement apt-daily-upgrade.service disabled
+    check_optional_service_enablement apt-daily.timer disabled
+    check_optional_service_enablement apt-daily-upgrade.timer disabled
+
+    if [ "$DISABLE_BLUETOOTH" = true ] ; then
+        check_optional_service_enablement hciuart.service disabled
+        check_optional_service_enablement bluetooth.service disabled
+    fi
+
+    if [ "$ENABLE_STATIC_IP" = true ] ; then
+        verify_file_contains_string_once "${OPTIMIZE_DHCP_CONF_HEADER}" "${OPTIMIZE_DHCP_CONF}"
+    fi
+    if [ "$DISABLE_IPv6" = true ] ; then
+        verify_file_contains_string_once "${OPTIMIZE_IPV6_CONF_HEADER}" "${OPTIMIZE_DHCP_CONF}"
+    fi
+    if [ "$DISABLE_BOOT_SCREEN" = true ] ; then
+        verify_file_contains_string_once "${OPTIMIZE_BOOT_CONF_HEADER}" "${RPI_BOOT_CONFIG_FILE}"
+    fi
+
+    if [ "$DISABLE_BOOT_LOGS_PRINT" = true ] ; then
+        for option in $OPTIMIZE_BOOT_CMDLINE_OPTIONS
+        do
+            verify_file_contains_string_once $option "${RPI_BOOT_CMDLINE_FILE}"
+        done
+    fi
 }
 
 optimize_boot_time() {
@@ -108,6 +165,7 @@ optimize_boot_time() {
   _optimize_ipv6_arp
   _optimize_handle_boot_screen
   _optimize_handle_boot_logs
+  _optimize_check
 
   echo "DONE: optimize_boot_time"
 }
