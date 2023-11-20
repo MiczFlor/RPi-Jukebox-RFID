1b53fbe9a017f9decbe643c9c5d3f65151255d92
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-11-20 23:37:02 +0100

    add checks for static ip config

--- installation/routines/optimize_boot_time.sh
+++ installation/routines/optimize_boot_time.sh
@@ -138,6 +138,9 @@ _optimize_check() {
 
     if [ "$ENABLE_STATIC_IP" = true ] ; then
         verify_file_contains_string_once "${OPTIMIZE_DHCP_CONF_HEADER}" "${OPTIMIZE_DHCP_CONF}"
+        verify_file_contains_string "${CURRENT_INTERFACE}" "${OPTIMIZE_DHCP_CONF}"
+        verify_file_contains_string "${CURRENT_IP_ADDRESS}" "${OPTIMIZE_DHCP_CONF}"
+        verify_file_contains_string "${CURRENT_GATEWAY}" "${OPTIMIZE_DHCP_CONF}"
     fi
     if [ "$DISABLE_IPv6" = true ] ; then
         verify_file_contains_string_once "${OPTIMIZE_IPV6_CONF_HEADER}" "${OPTIMIZE_DHCP_CONF}"
