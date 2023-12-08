554c0ed0b87717dc90e76794dd63f871ae83e1f6
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-12-08 08:26:19 +0100

    fix local libzmq build
    fix folder handling ZMQ_TMP_DIR.
    remove libsodium (use internal encryption lib tweetnacl).
    add cpu_count.
    add --disable-Werror.
    add sudo to make install.
    update log messaages.

--- installation/routines/setup_jukebox_core.sh
+++ installation/routines/setup_jukebox_core.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Constants
-ZMQ_TMP_DIR="libzmq"
+ZMQ_TMP_DIR="${HOME_PATH}/libzmq"
 ZMQ_PREFIX="/usr/local"
 ZMQ_VERSION="4.3.5"
 
@@ -48,30 +48,26 @@ _jukebox_core_configure_pulseaudio() {
 }
 
 _jukebox_core_build_libzmq_with_drafts() {
-  echo "  Building libzmq with drafts support" | tee /dev/fd/3
+  echo "    Building libzmq v${ZMQ_VERSION} with drafts support" | tee /dev/fd/3
+  local zmq_filename="zeromq-${ZMQ_VERSION}"
+  local zmq_tar_filename="${zmq_filename}.tar.gz"
+  local cpu_count=${CPU_COUNT:-$(python3 -c "import os; print(os.cpu_count())")}
 
-  LIBSODIUM_VERSION="1.0.18"
-  echo "    Building libsodium v${LIBSODIUM_VERSION}" | tee /dev/fd/3
-  { cd "${HOME_PATH}" && mkdir "${ZMQ_TMP_DIR}" && cd "${ZMQ_TMP_DIR}"; } || exit_on_error
-  wget --quiet https://github.com/jedisct1/libsodium/releases/download/${LIBSODIUM_VERSION}-RELEASE/libsodium-${LIBSODIUM_VERSION}.tar.gz
-  tar -zxvf libsodium-${LIBSODIUM_VERSION}.tar.gz
-  cd libsodium-${LIBSODIUM_VERSION} || exit_on_error
-  ./configure
-  make && make install
-
-  echo "    Building libzmq v${ZMQ_VERSION}" | tee /dev/fd/3
-  cd "${HOME}/${ZMQ_TMP_DIR}" || exit_on_error
-  wget --quiet https://github.com/zeromq/libzmq/releases/download/v${ZMQ_VERSION}/zeromq-${ZMQ_VERSION}.tar.gz -O libzmq.tar.gz
-  tar -xzf libzmq.tar.gz
-  zeromq-${ZMQ_VERSION}/configure --prefix=${ZMQ_PREFIX} --enable-drafts
-  make && make install
+  cd "${ZMQ_TMP_DIR}" || exit_on_error
+  wget --quiet https://github.com/zeromq/libzmq/releases/download/v${ZMQ_VERSION}/${zmq_tar_filename}
+  tar -xzf ${zmq_tar_filename}
+  rm -f ${zmq_tar_filename}
+  cd ${zmq_filename} || exit_on_error
+  ./configure --prefix=${ZMQ_PREFIX} --enable-drafts --disable-Werror
+  make -j${cpu_count} && sudo make install
 }
 
 _jukebox_core_download_prebuilt_libzmq_with_drafts() {
-  echo "    Download pre-compiled libzmq"
+  echo "    Download pre-compiled libzmq with drafts support"
   local ZMQ_TAR_FILENAME="libzmq.tar.gz"
   ARCH=$(get_architecture)
 
+  cd "${ZMQ_TMP_DIR}" || exit_on_error
   wget --quiet https://github.com/pabera/libzmq/releases/download/v${ZMQ_VERSION}/libzmq5-${ARCH}-${ZMQ_VERSION}.tar.gz -O ${ZMQ_TAR_FILENAME}
   tar -xzf ${ZMQ_TAR_FILENAME}
   rm -f ${ZMQ_TAR_FILENAME}
@@ -89,12 +85,12 @@ _jukebox_core_build_and_install_pyzmq() {
   echo "  Install pyzmq with libzmq-drafts to support WebSockets" | tee /dev/fd/3
 
   if ! pip list | grep -F pyzmq >> /dev/null; then
-    { cd "${HOME_PATH}" && mkdir "${ZMQ_TMP_DIR}" && cd "${ZMQ_TMP_DIR}"; } || exit_on_error
 
     if [[ $(uname -m) == "armv6l" ]]; then
       _show_slow_hardware_message
     fi
 
+    mkdir -p "${ZMQ_TMP_DIR}" || exit_on_error
     if [ "$BUILD_LIBZMQ_WITH_DRAFTS_ON_DEVICE" = true ] ; then
       _jukebox_core_build_libzmq_with_drafts
     else
@@ -151,8 +147,8 @@ _jukebox_core_check() {
 _run_setup_jukebox_core() {
     _jukebox_core_install_os_dependencies
     _jukebox_core_install_python_requirements
-    _jukebox_core_configure_pulseaudio
     _jukebox_core_build_and_install_pyzmq
+    _jukebox_core_configure_pulseaudio
     _jukebox_core_install_settings
     _jukebox_core_register_as_service
     _jukebox_core_check
