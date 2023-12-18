e85b2944008e738157fa753579bb406f1d42f16f
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2023-12-18 19:03:25 +0100

    Bring back spotify backend

---
+++ src/jukebox/components/player/backends/spotify/interfacing_spotify.py
@@ -0,0 +1,121 @@
+# Copyright: 2022
+# SPDX License Identifier: MIT License
+
+import logging
+import os.path
+import os
+
+from ruamel import yaml
+from spotipy import CacheFileHandler
+
+import jukebox.plugs as plugin
+import jukebox.cfghandler
+
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy
+
+logger = logging.getLogger('jb.spotify')
+cfg = jukebox.cfghandler.get_handler('jukebox')
+
+
+def sanitize(path: str):
+    return os.path.normpath(path).lstrip('./')
+
+
+class SPOTBackend:
+    def __init__(self, player_status):
+        host = cfg.getn('playerspot', 'host')
+        self.player_status = player_status
+        self.cache_handler = CacheFileHandler(cache_path='../../shared/spotify/')
+        self.cache_file = '../../shared/spotify/.spotipyoauthcache'
+        self.client_id = cfg.setndefault('playerspot', 'client_id', value='Phoniebox')
+        self.client_secret = cfg.setndefault('playerspot', 'client_secret', value='Phoniebox_secret')
+        self.redirect_uri = cfg.setndefault('playerspot', 'callback_url',
+                                            value='http://localhost:3001')
+
+        spot_scope = "user-read-playback-state,user-modify-playback-state"
+        self.auth_manager = SpotifyOAuth(open_browser=False, scope=spot_scope, client_id=self.client_id, client_secret=self.client_id, redirect_uri=self.redirect_uri, cache_path=sanitize(self.cache_file))
+        self.auth_uri = self.auth_manager.get_authorize_url()
+        logger.info(f"Please log in here: {self.auth_uri}")
+
+        #self.collection_file_location = cfg.setndefault('playerspot', 'collection_file',
+        #                                                value="../../shared/audio/spotify/spotify_collection.yaml")
+        #self.spotify_collection_data = self._read_data_file()
+
+    @plugin.tag
+    def init_spotclient(self, spot_code=None):
+        token_info = self.auth_manager.get_cached_token()
+        logger.debug(f"Token Info: {token_info}")
+
+        if token_info:
+            logger.debug("Found cached token for Spotify Client!")
+            access_token = token_info['access_token']
+        else:
+            # ToDo: implement this within the web app
+            token_info = self.auth_manager.get_access_token(spot_code)
+            access_token = token_info['access_token']
+
+        if access_token:
+            self.spot_client = spotipy.Spotify(access_token)
+            self.auth_code = cfg.setndefault('playerspot', 'auth_code', value=access_token)
+
+    def _read_data_file(self) -> dict:
+        try:
+            with open(self.collection_file_location, "r") as collection_file:
+                return yaml.safe_load(collection_file.read())
+        except Exception as err:
+            logger.error(f"Could not open spotify collection file {self.collection_file_location}")
+            logger.debug(f"Error: {err}")
+            logger.debug("Continuing with empty dictionary")
+            return {}
+
+    def play(self):
+        return self.spot_client.start_playback()
+
+    def pause(self):
+        return self.spot_client.pause_playback()
+
+    def stop(self):
+        return self.spot_client.pause_playback()
+
+    def prev(self):
+        return self.spot_client.previous_track()
+
+    def next(self):
+        return self.spot_client.next_track()
+
+    def toggle(self):
+        pass
+
+    def get_queue(self):
+        pass
+
+    @plugin.tag
+    def play_uri(self, uri: str, **kwargs):
+        """Decode URI and forward play call
+
+        spotify:playlist:0
+            --> search in the yaml-file for the type "playlist" and play the first uri
+        """
+        player_type, index = uri.split(':', 1)
+        if player_type != 'spotify':
+            raise KeyError(f"URI prefix must be 'spotify' not '{player_type}")
+
+        return self.spot_client.start_playback(context_uri=uri)
+
+    @plugin.tag
+    def get_status(self):
+        return self.spot_client.current_user()
+
+    # -----------------------------------------------------
+    # Queue / URI state  (save + restore e.g. random, resume, ...)
+
+    def save_state(self):
+        """Save the configuration and state of the current URI playback to the URIs state file"""
+        pass
+
+    def _restore_state(self):
+        """
+        Restore the configuration state and last played status for current active URI
+        """
+        pass
--- src/jukebox/components/player/plugin/__init__.py
+++ src/jukebox/components/player/plugin/__init__.py
@@ -9,9 +9,12 @@ from typing import Optional
 import jukebox.plugs as plugin
 import jukebox.cfghandler
 from components.player.backends.mpd.interfacing_mpd import MPDBackend
+from components.player.backends.spotify.interfacing_spotify import SPOTBackend
 from components.player.core import PlayerCtrl
-from components.player.core.player_content import PlayerData
 from components.player.core.player_status import PlayerStatus
+
+from components.player.core.player_content import PlayerData
+
 
 logger = logging.getLogger('jb.player')
 cfg = jukebox.cfghandler.get_handler('jukebox')
@@ -27,6 +30,7 @@ player_status: PlayerStatus
 
 # The various backends
 backend_mpd: Optional[MPDBackend] = None
+backend_spot: Optional[SPOTBackend] = None
 
 
 def start_event_loop(loop: asyncio.AbstractEventLoop):
@@ -50,6 +54,17 @@ def register_mpd():
     player_arbiter.register('mpd', backend_mpd)
 
 
+def register_spotify():
+    global backend_spot
+    global player_arbiter
+    global player_status
+
+    backend_spot = SPOTBackend(player_status)
+    # Register with plugin interface to call directly
+    plugin.register(backend_spot, package='player', name='spotify')
+    player_arbiter.register('spotify', backend_spot)
+
+
 @plugin.initialize
 def initialize():
     global event_loop
@@ -69,6 +84,7 @@ def initialize():
     player_content = PlayerData()
 
     # Create and register the players (this is explicit for the moment)
+    register_spotify()
     register_mpd()
 
     plugin.register(player_arbiter, package='player', name='ctrl')
