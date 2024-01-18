a4e39f4bbc2ce5a79652320fbbdb85b82ab9eb3b
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2024-01-18 23:42:23 +0100

    fix timer service unit definition

--- installation/routines/setup_autohotspot_NetworkManager.sh
+++ installation/routines/setup_autohotspot_NetworkManager.sh
@@ -23,7 +23,6 @@ _install_autohotspot_NetworkManager() {
     sudo sed -i "s|%%AUTOHOTSPOT_IP%%|${AUTOHOTSPOT_IP}|g" "${AUTOHOTSPOT_TARGET_PATH}"
     sudo sed -i "s|%%IP_WITHOUT_LAST_SEGMENT%%|${ip_without_last_segment}|g" "${AUTOHOTSPOT_TARGET_PATH}"
     sudo sed -i "s|%%AUTOHOTSPOT_TIMER_NAME%%|${AUTOHOTSPOT_TIMER}|g" "${AUTOHOTSPOT_TARGET_PATH}"
-    
     sudo chmod +x "${AUTOHOTSPOT_TARGET_PATH}"
 
     sudo cp "${AUTOHOTSPOT_NETWORKMANAGER_RESOURCES_PATH}"/autohotspot.service "${AUTOHOTSPOT_SERVICE_PATH}"
@@ -32,7 +31,7 @@ _install_autohotspot_NetworkManager() {
     #sudo chmod 644 "${AUTOHOTSPOT_SERVICE_PATH}"
 
     sudo cp "${AUTOHOTSPOT_NETWORKMANAGER_RESOURCES_PATH}"/autohotspot.timer "${AUTOHOTSPOT_TIMER_PATH}"
-    sudo sed -i "s|%%AUTOHOTSPOT_SERVICE%%|${AUTOHOTSPOT_SERVICE_PATH}|g" "${AUTOHOTSPOT_TIMER_PATH}"
+    sudo sed -i "s|%%AUTOHOTSPOT_SERVICE%%|${AUTOHOTSPOT_SERVICE}|g" "${AUTOHOTSPOT_TIMER_PATH}"
     #sudo chown root:root "${AUTOHOTSPOT_SERVICE_PATH}"
     #sudo chmod 644 "${AUTOHOTSPOT_SERVICE_PATH}"
 
@@ -83,13 +82,12 @@ _autohotspot_check_NetworkManager() {
     verify_file_contains_string "ap_ip='${AUTOHOTSPOT_IP}" "${AUTOHOTSPOT_TARGET_PATH}" #intentional "open end"
     verify_file_contains_string "ap_gate='${ip_without_last_segment}" "${AUTOHOTSPOT_TARGET_PATH}" #intentional "open end"
     verify_file_contains_string "timer_service_name='${AUTOHOTSPOT_TIMER}'" "${AUTOHOTSPOT_TARGET_PATH}"
-    
 
     verify_files_exists "${AUTOHOTSPOT_SERVICE_PATH}"
     verify_file_contains_string "ExecStart=${AUTOHOTSPOT_TARGET_PATH}" "${AUTOHOTSPOT_SERVICE_PATH}"
 
     verify_files_exists "${AUTOHOTSPOT_TIMER_PATH}"
-    verify_file_contains_string "Unit=${AUTOHOTSPOT_SERVICE_PATH}" "${AUTOHOTSPOT_TIMER_PATH}"
+    verify_file_contains_string "Unit=${AUTOHOTSPOT_SERVICE}" "${AUTOHOTSPOT_TIMER_PATH}"
 }
 
 _run_setup_autohotspot_NetworkManager() {
