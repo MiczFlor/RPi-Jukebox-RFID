08cfcb7933eae45fdf5faeb3179c7d1eafa44c39
pabera <1260686+pabera@users.noreply.github.com>
2024-01-23 23:28:34 +0100

    refactor: introduce optimized defaults, opinionated improvements

--- installation/routines/optimized_defaults.sh
+++ installation/routines/optimized_defaults.sh
@@ -2,16 +2,6 @@
 
 # Reference: https://panther.software/configuration-code/raspberry-pi-3-4-faster-boot-time-in-few-easy-steps/
 
-_optimize_disable_irrelevant_services() {
-  ./../options/systemctl_services.sh disable
-}
-
-_optimize_handle_bluetooth() {
-  if [ "$DISABLE_BLUETOOTH" = true ] ; then
-    ./../options/bluetooth.sh disable
-  fi
-}
-
 # TODO: Allow options to enable/disable wifi, Dynamic/Static IP etc.
 _optimize_static_ip() {
   # Static IP Address and DHCP optimizations
@@ -28,28 +18,18 @@ _optimize_ipv6_arp() {
   fi
 }
 
-_optimize_handle_boot_screen() {
-  if [ "$DISABLE_BOOT_SCREEN" = true ] ; then
-    ./../options/boot_screen.sh disable
-  fi
-}
-
-_optimize_handle_boot_logs() {
-  if [ "$DISABLE_BOOT_LOGS_PRINT" = true ] ; then
-    ./../options/boot_logs.sh disable
-  fi
-}
-
 _run_optimized_defaults() {
-    _optimize_disable_irrelevant_services
-    _optimize_handle_bluetooth
+    ./../options/systemctl_services.sh disable
+    ./../options/bluetooth.sh disable
+    ./../options/boot_screen.sh disable
+    ./../options/boot_logs.sh disable
+    ./../options/ssh_qos.sh disable
+
     _optimize_static_ip
     _optimize_ipv6_arp
-    _optimize_handle_boot_screen
-    _optimize_handle_boot_logs
     _optimize_check
 }
 
 optimized_defaults() {
-    run_with_log_frame _run_optimized_defaults "Optimize boot time"
+    run_with_log_frame _run_optimized_defaults "Optimize boot time and system settings"
 }
