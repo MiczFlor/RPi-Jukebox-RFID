7c87cea849652f544b431ad542887678936c9491
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2024-01-31 13:48:17 +0100

    fixed parameter passing

--- installation/components/setup_rfid_reader.sh
+++ installation/components/setup_rfid_reader.sh
@@ -11,4 +11,4 @@ cd "$SCRIPT_DIR" || { echo "Could not change to script directory"; exit 1; }
 PROJECT_ROOT="$SCRIPT_DIR"/../../
 source "${PROJECT_ROOT}"/.venv/bin/activate || { echo "ERROR: Failed to activate virtual environment for python"; exit 1; }
 
-python "${PROJECT_ROOT}"/src/jukebox/components/rfid/hardware/run_register_rfid_reader.py
+python "${PROJECT_ROOT}"/src/jukebox/components/rfid/hardware/run_register_rfid_reader.py $@
