654d031a104506d14f08f66a80c64b42342a00b1
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2024-02-01 02:49:25 +0100

    move scripts to python root again (failing import paths). fixed absolute path in caller scripts

--- installation/components/setup_rfid_reader.sh
+++ installation/components/setup_rfid_reader.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-# Runner script for setup rfid reader to ensure
+# Runner script for sniffer tool to ensure
+# - correct venv activation
 # - independent from working directory
 
 # Change working directory to location of script
 SOURCE=${BASH_SOURCE[0]}
 SCRIPT_DIR="$(dirname "$SOURCE")"
-cd "$SCRIPT_DIR" || { echo "Could not change to script directory"; exit 1; }
+PROJECT_ROOT="$(realpath "$SCRIPT_DIR"/../..)"
 
-PROJECT_ROOT="$SCRIPT_DIR"/../../
+cd "$PROJECT_ROOT" ||  { echo "Could not change directory"; exit 1; }
 source "${PROJECT_ROOT}"/.venv/bin/activate || { echo "ERROR: Failed to activate virtual environment for python"; exit 1; }
 
-python "${PROJECT_ROOT}"/src/jukebox/components/rfid/hardware/run_register_rfid_reader.py $@
+cd "${PROJECT_ROOT}"/src/jukebox ||  { echo "Could not change directory"; exit 1; }
+python run_register_rfid_reader.py $@
