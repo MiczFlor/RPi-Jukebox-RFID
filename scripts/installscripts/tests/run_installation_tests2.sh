f1eb030ef764d839ea9d59ca1ec682f0b33fc672
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2024-03-12 22:50:15 +0000

    improve readability and update tests parameter

--- scripts/installscripts/tests/run_installation_tests2.sh
+++ scripts/installscripts/tests/run_installation_tests2.sh
@@ -15,20 +15,42 @@ echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selecti
 
 # Run installation (in interactive mode)
 # y confirm interactive
-# n dont configure wifi
-# n dont configure autohotspot
-# y use default audio iface
-# n no spotify
-# y configure mpd
-# y audio default location
-# y use gpio
+# n configure wifi (extra ENTER)
+# n configure autohotspot (extra ENTER)
+# y use default audio iface (extra ENTER)
+# n spotify (extra ENTER)
+# y configure mpd (extra ENTER)
+# y audio default location (extra ENTER)
+# y use gpio (extra ENTER)
+# y start installation
 # y RFID registration
 # 2 use RC522 reader
 # y, reader is connected
 # y, use legacy readermode
-# n No reboot
+# n reboot
 
-./../install-jukebox.sh <<< $'y\nn\n\nn\n\ny\n\nn\n\ny\n\ny\n\ny\n\ny\ny\n2\ny\ny\nn\n'
+./../install-jukebox.sh <<< "y
+n
+
+n
+
+y
+
+n
+
+y
+
+y
+
+y
+
+y
+y
+2
+y
+y
+n
+"
 INSTALLATION_EXITCODE=$?
 
 # Test installation
