1477913fe94b06267b475469a95a2ca70b3e4f46
pabera <1260686+pabera@users.noreply.github.com>
2024-03-15 22:38:52 +0100

    fix: Flake8 issue

--- src/jukebox/jukebox/playlistgenerator.py
+++ src/jukebox/jukebox/playlistgenerator.py
@@ -275,7 +275,12 @@ class PlaylistCollector:
             logger.error(f" {e.__class__.__name__}: {e}")
         else:
             for m in content:
-                self.playlist.append({'type': TYPE_DECODE[m.filetype], 'name': m.name, 'path': m.path, 'relpath': os.path.relpath(m.path, self._music_library_base_path)})
+                self.playlist.append({
+                    'type': TYPE_DECODE[m.filetype],
+                    'name': m.name,
+                    'path': m.path,
+                    'relpath': os.path.relpath(m.path, self._music_library_base_path)
+                })
 
     def _parse_nonrecusive(self, path='.'):
         return [x.path for x in self._get_directory_content(path) if x.filetype != TYPE_DIR]
