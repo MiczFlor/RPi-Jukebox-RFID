c23b4b604cbd0ca1975f37bd9f70a74a9e80e393
s-martin <s-martin@users.noreply.github.com>
2024-03-20 22:18:25 +0000

    check, if cmd does not exisr

--- run_markdownlint.sh
+++ run_markdownlint.sh
@@ -10,4 +10,4 @@ PROJECT_ROOT="$SCRIPT_DIR"
 cd "$PROJECT_ROOT" || { echo "Could not change directory"; exit 1; }
 
 # Run markdownlint-cli2
-./src/webapp/node_modules/.bin/markdownlint-cli2 --config .markdownlint-cli2.yaml "#node_modules"
+./src/webapp/node_modules/.bin/markdownlint-cli2 --config .markdownlint-cli2.yaml "#node_modules" || { echo "ERROR: markdownlint-cli2 not found"; exit 1; }
