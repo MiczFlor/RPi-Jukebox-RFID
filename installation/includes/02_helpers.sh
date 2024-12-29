9703c99d87e5c50ce3a71eeb19f0b08270d8d64f
pabera <1260686+pabera@users.noreply.github.com>
2024-12-29 13:12:50 +0100

    fix: call the right function name to check on debian version

--- installation/includes/02_helpers.sh
+++ installation/includes/02_helpers.sh
@@ -84,7 +84,7 @@ is_debian_based() {
     fi
 }
 
-get_debian_version_number() {
+_get_debian_version_number() {
     if [ "$(is_debian_based)" = true ]; then
         local debian_version_number=$( . /etc/os-release; printf '%s\n' "$VERSION_ID"; )
         echo "$debian_version_number"
