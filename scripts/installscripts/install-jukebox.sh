bf5be9a466e7fa681c11e585b669a8530151438e
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2025-11-19 00:39:15 +0100

    fix: log lgpio build

--- scripts/installscripts/install-jukebox.sh
+++ scripts/installscripts/install-jukebox.sh
@@ -983,7 +983,7 @@ install_main() {
     fi
 
     # always build lgpio as the pypi binaries are incomplete (armv6) or broken (bullseye)
-    mkdir -p tmp && cd tmp && wget -q http://abyz.me.uk/lg/lg.zip && unzip lg.zip > /dev/null && cd lg && make > /dev/null && sudo make install > /dev/null
+    mkdir -p tmp && cd tmp && wget -q http://abyz.me.uk/lg/lg.zip && unzip lg.zip > /dev/null && cd lg && make && sudo make install
     cd "${HOME_DIR}" && sudo rm -rf tmp > /dev/null
     local pip_install_options="--no-binary=lgpio"
 
