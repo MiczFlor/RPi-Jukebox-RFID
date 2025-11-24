9d6be6dff049f142e438b3004dfc7d00b96af49a
s-martin <s-martin@users.noreply.github.com>
2025-11-24 21:05:52 +0100

    Fix error in URL

--- scripts/installscripts/install-jukebox.sh
+++ scripts/installscripts/install-jukebox.sh
@@ -966,8 +966,8 @@ install_main() {
 
         # not yet available on apt.mopidy.com, so install manually
         local arch=$(dpkg --print-architecture)
-        local gst_plugin_spotify_name="gst-plugin-spotify_0.15.0-alpha.1-4_${arch}.deb"
-        wget -q https://github.com/kingosticks/gst-plugins-rs-build/releases/download/gst-plugin-spotify_0.15.0-alpha.1-4/${gst_plugin_spotify_name}
+        local gst_plugin_spotify_name="gst-plugin-spotify_0.15.0.alpha.1-4_${arch}.deb"
+        wget -q https://github.com/kingosticks/gst-plugins-rs-build/releases/download/gst-plugin-spotify_0.15.0.alpha.1-4/${gst_plugin_spotify_name}
         ${apt_get} install ./${gst_plugin_spotify_name}
         sudo rm -f ${gst_plugin_spotify_name}
 
